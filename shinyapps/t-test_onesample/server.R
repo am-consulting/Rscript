004cb87d3850ca36ba6fa8e50c612e6a6144f7bf
am-consulting <contact@am-consulting.co.jp>
2016-06-15 16:28:16 +0900

    t検定1サンプル新規アップロード
    t検定1サンプル新規アップロード

---
+++ shinyapps/t-test_onesample/server.R
@@ -0,0 +1,281 @@
+library(shiny)
+library(dplyr)
+library(nortest)
+options(shiny.maxRequestSize = 0.5 * 1024 ^ 2)
+shinyServer(function(input, output , session) {
+  uploadFile <- reactive({
+    inFile <- input$file
+    if (is.null(inFile)) {
+      return(NULL)
+    } else{
+      origData <<- read.csv(
+        inFile$datapath,
+        header = input$header,
+        sep = input$sep,
+        quote = input$quote,
+        check.names = input$chkname,
+        stringsAsFactors = F
+      )
+    }
+  })
+  
+  output$targetData <- renderUI({
+    uploadFile()
+    if (is.null(input$file))
+      return(NULL)
+    selectInput("targetData",
+                "Select as Sample",
+                colnames(origData),
+                selected = colnames(origData)[1])
+  })
+  
+  output$selectrow <- renderUI({
+    uploadFile()
+    if (is.null(input$file))
+      return(NULL)
+    sliderInput(
+      "selectedRow",
+      label = "Select Rows",
+      min = 1,
+      max = nrow(origData),
+      value =  c(1, nrow(origData)),
+      step = 1
+    )
+  })
+  
+  trueMean <- reactive({
+    if (is.null(input$targetData))
+      return(NULL)
+    targetcol <-
+      which(colnames(origData) == input$targetData)
+    dataset <-
+      data.frame(ID = seq(1, nrow(origData)),
+                 origData[, targetcol],
+                 check.names = F)
+    dataset <-
+      dataset %>% filter(input$selectedRow[1] <= dataset[, 1],
+                         dataset[, 1] <= input$selectedRow[2])
+    colnames(dataset)[2] <- colnames(origData)[targetcol]
+    dataset <<- dataset
+    output$mu <- renderUI({
+      textInput("truemean",
+                label = "True Mean",
+                value = mean(dataset[, 2]))
+    })
+    altList <- c("two.sided", "less", "greater")
+    output$alternative <- renderUI({
+      selectInput(
+        "alternative",
+        label = "Alternative",
+        choices = altList ,
+        selected = altList[1]
+      )
+    })
+  })
+  
+  resultOutput <- reactive({
+    if (is.null(input$targetData))
+      return(NULL)
+    summaryDF <- data.frame()
+    x <<- dataset[, 2]
+    iii <- 1
+    #unbiased variance
+    summaryDF[1, iii] <- var(x)
+    #unbiased deviation
+    summaryDF[2, iii] <- sd(x)
+    #sample variance
+    summaryDF[3, iii] <-
+      var(x) * (length(x) - 1) / length(x)
+    #sample standard deviation
+    summaryDF[4, iii] <- sqrt(summaryDF[3, iii])
+    summaryDF[5, iii] <- mean(x)
+    summaryDF[6, iii] <- median(x)
+    summaryDF[7, iii] <- max(x)
+    summaryDF[8, iii] <- min(x)
+    summaryDF[9, iii] <- ad.test(x)$p.value
+    summaryDF[9, iii + 1] <- paste(ad.test(x)$method, ": p-value")
+    summaryItem <-
+      c(
+        "Unbiased Variance",
+        "Unbiased Deviation",
+        "Sample Variance",
+        "Sample Standard Deviation",
+        "Mean",
+        "Median",
+        "Max",
+        "Min",
+        "Normality"
+      )
+    summaryDF <-
+      data.frame(ID = seq(1, nrow(summaryDF)), Item = summaryItem, summaryDF)
+    colnames(summaryDF) <- c("ID", "Item", "Value", "Remark")
+    summaryDF <<- summaryDF
+    
+    output$scatter <- renderPlot({
+      par(mar = c(4, 4, 2, 1))
+      collist <- c("red")
+      plot(
+        x,
+        x,
+        type = "p",
+        col = "black",
+        xlab = colnames(dataset)[2] ,
+        ylab = colnames(dataset)[2] ,
+        panel.first = grid(
+          nx = NULL,
+          ny = NULL,
+          lty = 2,
+          equilogs = T
+        ) ,
+        main = "Scatter Plot",
+        cex.axis = 1,
+        cex.lab = 1,
+        cex.main = 1,
+        asp = 1
+      )
+    })
+    
+    output$hist <- renderPlot({
+      par(mar = c(5, 4, 3, 3))
+      hist(
+        x,
+        breaks = "sturges",
+        cex.axis = 1,
+        cex.lab = 1,
+        cex.main = 1,
+        main = colnames(dataset)[2],
+        xlab = "",
+        col = "#DCDCDC"
+      )
+    })
+    
+    output$qq <- renderPlot({
+      qqnorm(x,
+             panel.first = grid(
+               nx = NULL,
+               ny = NULL,
+               lty = 2,
+               equilogs = T
+             ))
+      qqline(x, col = "red")
+    })
+    
+    if (input$truemean == "") {
+      muvalue <- 0
+    } else{
+      muvalue <- as.numeric(input$truemean)
+    }
+    resultttest <-
+      t.test(x, mu = muvalue, alternative = input$alternative) # "two.sided", "less", "greater"
+    attributes(resultttest)
+    ttestDF <-
+      data.frame(
+        Item = c(
+          "t",
+          "df",
+          "p-value",
+          "CI=95%.L",
+          "CI=95%.U",
+          "mean of x",
+          "null value"
+        ),
+        Value = c(
+          resultttest$statistic,
+          resultttest$parameter,
+          resultttest$p.value,
+          resultttest$conf.int[1],
+          resultttest$conf.int[2],
+          resultttest$estimate,
+          resultttest$null.value
+        )
+      )
+    ttestDF <- data.frame(ID = seq(1, nrow(ttestDF)), ttestDF)
+    
+    output$ttestDF <- DT::renderDataTable(
+      ttestDF,
+      rownames = F,
+      caption = paste(
+        "Table 2: ",
+        resultttest$method,
+        ":",
+        resultttest$alternative
+      ),
+      options = list(
+        autoWidth = T,
+        info = T,
+        lengthChange = T,
+        ordering = T,
+        searching = T,
+        scrollX = T,
+        lengthMenu = list(c(-1, 1), c("All", "1")),
+        orderClasses = TRUE,
+        order = list(list(0, "asc"))
+      )
+    )
+    
+    output$summaryDF <- DT::renderDataTable(
+      summaryDF,
+      rownames = F,
+      caption = paste("Table 1: Statistical Summary"),
+      options = list(
+        autoWidth = T,
+        info = T,
+        lengthChange = T,
+        ordering = T,
+        searching = T,
+        scrollX = T,
+        lengthMenu = list(c(-1, 1), c("All", "1")),
+        orderClasses = TRUE,
+        order = list(list(0, "asc"))
+      )
+    )
+  
+  origDataset <-
+    data.frame(ID = seq(1, nrow(origData)), origData, check.names = F)
+  
+  output$DF <- DT::renderDataTable(
+    origDataset,
+    rownames = F,
+    caption = paste("Table 3: Original Dataset"),
+    options = list(
+      autoWidth = T,
+      info = T,
+      lengthChange = T,
+      ordering = T,
+      searching = T,
+      scrollX = T,
+      lengthMenu = list(c(10, -1, 1), c("10", "All", "1")),
+      orderClasses = TRUE,
+      order = list(list(0, "asc"))
+    )
+  )
+  })
+  
+  output$completiontime <- renderText({
+    if (is.null(input$targetData))
+      return(NULL)
+    trueMean()
+    resultOutput()
+    paste("Completion:" ,
+          as.character(as.POSIXlt(Sys.time(), "GMT")), "(UTC)")
+  })
+  
+  output$remarktext <- renderUI({
+    str <- "<hr>
+    <b>Remarks</b><br>
+    <ol>
+    <li><a href=\"http://www.saecanet.com\" target=\"_blank\">SaECaNet</a></li>
+    <li>Other apps <a href=\"http://webapps.saecanet.com\" target=\"_blank\">SaECaNet - Web Applications</a></li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$history <- renderUI({
+    str <- "<hr>
+    <b>History</b><br>
+    <ol>
+    <li>2016-06-15:ver.1.0.0</li>
+    </ol>"
+    HTML(str)
+  })
+})
---
+++ shinyapps/t-test_onesample/ui.R
@@ -0,0 +1,114 @@
+library(shiny)
+shinyUI(fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet",
+              href = "http://fonts.googleapis.com/css?family=Roboto+Condensed"),
+    # favicon
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-144-precomposed.png",
+              sizes = "144x144"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-114-precomposed.png",
+              sizes = "114x114"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-72-precomposed.png",
+              sizes = "72x72"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-52-precomposed.png",
+              sizes = "52x52"),
+    tags$link(rel = "shortcut icon",
+              href = "http://www.saecanet.com/subfolder/ico/favicon.png"),
+    # favicon
+    tags$style("body{font-family: 'Roboto Condensed', sans-serif;}"),
+    tags$script(
+      '!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");'
+    )
+  ),
+  headerPanel("t-test:one sample."),
+  textOutput("completiontime"),
+  fluidRow(
+    column(
+      2,
+      wellPanel(
+        fileInput(
+          'file',
+          'Choose CSV File',
+          accept = c('text/csv',
+                     'text/comma-separated-values,text/plain',
+                     '.csv')
+        ),
+        tags$script('$( "#file" ).on( "click", function() {
+                    this.value = null;
+                    });'),
+         uiOutput("targetData"),
+        uiOutput("selectrow"),
+        uiOutput("mu"),
+        uiOutput("alternative"),
+        tags$hr(),
+        checkboxInput('header', 'Header', TRUE),
+        radioButtons('sep', 'Separator',
+                     c(
+                       Comma = ',',
+                       Semicolon = ';',
+                       Tab = '\t'
+                     ),
+                     ','),
+        radioButtons(
+          'quote',
+          'Quote',
+          c(
+            'None' = '',
+            'Double Quote' = '"',
+            'Single Quote' = "'"
+          ),
+          ''
+        ),
+        radioButtons('chkname', 'Check Names',
+                     c('True' = 'T',
+                       'False' = 'F'),
+                     'F')
+      )
+    )  ,
+    column(
+      8,
+      fluidRow(
+        column(4, plotOutput("hist")),
+        column(4, plotOutput("scatter")),
+        column(4, plotOutput("qq"))
+      ),
+      tags$hr()
+      ,
+      fluidRow(
+        conditionalPanel(
+          condition = "output.completiontime!=''",
+          column(
+            12,
+            DT::dataTableOutput("summaryDF"),
+            tags$hr(),
+            DT::dataTableOutput("ttestDF"),
+            tags$hr(),
+            DT::dataTableOutput("DF")
+            
+          )
+        )
+      ),
+      DT::dataTableOutput("dt")
+      ,
+      htmlOutput("remarktext"),
+      htmlOutput("history")
+    ),
+    column(
+      2,
+      a(
+        "@AMC2_Japan",
+        class = "twitter-timeline"
+        ,
+        href = "https://twitter.com/AMC2_Japan"
+        ,
+        "data-widget-id" = "449799943780200448",
+        width = "100%",
+        height = "2000"
+      )
+    )
+  )
+))
