aa52c8ec412886d2ba3b988b7f8909ff8c472a18
am-consulting <contact@am-consulting.co.jp>
2016-06-16 00:43:30 +0900

    ヒストリカルFXデータ新規アップロード
    ヒストリカルFXデータ新規アップロード

---
+++ shinyapps/historicalFXData/server.R
@@ -0,0 +1,290 @@
+library(shiny)
+library(tseries)
+library(forecast)
+library(DT)
+library(nortest)
+shinyServer(function(input, output, session)
+{
+  reactiveData <- reactive({
+    # tmp <-
+    #   origData[, c(1, grep(
+    #     paste("\\b", input$currency , "\\b", sep = ""),
+    #     colnames(origData)
+    #   ))]
+    tmp <-origData[, c(1, which(input$currency==colnames(origData)))]
+    tmp <- na.omit(tmp)
+    startDate <- input$dateRange[1]
+    endDate <-
+      max(min(input$dateRange[2], tmp[nrow(tmp), 1]), tmp[1, 1] + 365)
+    if (startDate >= endDate) {
+      startDate <- endDate - 365
+    }
+    tmp <- subset(tmp, startDate <= tmp[, 1] & tmp[, 1] <=
+                    endDate)
+    buf <- colnames(tmp)
+    if (input$datatype == 2)
+      #1st diff
+    {
+      tmp <-
+        data.frame(tmp[-1, 1], diff(tmp[, 2], lag = 1, differences = 1),
+                   check.names = F)
+    }
+    if (input$datatype == 3)
+      #2nd diff
+    {
+      tmp <-
+        data.frame(tmp[-c(1, 2), 1], diff(tmp[, 2], lag = 1, differences = 2),
+                   check.names = F)
+    }
+    colnames(tmp) <- buf
+    plotData <<- tmp
+    x <- tmp[, 2]
+    SV <- var(x) * (length(x) - 1) / length(x) #sample variance
+    UV <- var(x)#unbiased variance
+    SSD <- sqrt(SV)#sample standard deviation
+    minData <- tail(subset(tmp, min(tmp[, 2]) == tmp[, 2]), 1)
+    maxData <- tail(subset(tmp, max(tmp[, 2]) == tmp[, 2]), 1)
+    statistic <-
+      c(
+        "n",
+        "mean( x )",
+        "median( x )",
+        "min( x )",
+        "max( x )",
+        "sd( x )",
+        "Sample Standard Deviation",
+        "var( x )",
+        "Sample Variance",
+        "adf.test( x )",
+        "ad.test( x )"
+      )
+    result <-
+      c(length(x), round(
+        c(
+          mean(x),
+          median(x),
+          minData[, 2],
+          maxData[, 2],
+          sd(x),
+          SSD  ,
+          UV ,
+          SV ,
+          adf.test(x)$p.value,
+          ad.test(x)$p.value
+        ),
+        3
+      ))
+    remark <-
+      c(
+        "-",
+        "-",
+        "-",
+        format(minData[, 1], "%Y-%m-%d"),
+        format(maxData[, 1], "%Y-%m-%d"),
+        "-",
+        "-",
+        "-",
+        "-",
+        "p-value",
+        "p-value"
+      )
+    statisticTable <-
+      data.frame(
+        Statistic = statistic,
+        Result = result,
+        Remark = remark,
+        stringsAsFactors = F,
+        check.names = F
+      )
+    
+    variance <- function(x) {
+      tmpx <- sqrt(var(x) * (length(x) - 1) / length(x))
+      value <- floor(1000 * as.numeric(tmpx) + 0.5) / 1000
+      return(value)
+    }
+    
+    buffxData <- tail(origData, 100)
+    buffxData <- buffxData[, colSums(is.na(buffxData)) == 0]
+    meanValue = as.vector(colMeans(buffxData[, -1]), mode = 'numeric')
+    sdValue = as.vector(apply(buffxData[, -1], 2, variance), mode = 'numeric')
+    latest1Data <-
+      as.vector(buffxData[nrow(buffxData) - 0, -1], mode = 'numeric')
+    latest2Data <-
+      as.vector(buffxData[nrow(buffxData) - 1, -1], mode = 'numeric')
+    tmpDataset <- data.frame(
+      Currency = colnames(buffxData[, -1]),
+      latest1 = latest1Data,
+      latest2 = latest2Data,
+      Median = as.vector(apply(buffxData[, -1], 2, median), mode = 'numeric'),
+      SampleStandardDeviation = sdValue,
+      Max = as.vector(apply(buffxData[, -1], 2, max), mode = 'numeric'),
+      Min = as.vector(apply(buffxData[, -1], 2, min), mode = 'numeric'),
+      VariationCoefficient = sdValue / meanValue * 100,
+      FromTheDayBefore = (latest1Data - latest2Data) / latest2Data * 100,
+      row.names = NULL
+    )
+    tmpDataset[, c(5, 8, 9)] <-
+      floor(1000 * (tmpDataset[, c(5, 8, 9)]) + 0.5) / 1000
+    colnames(tmpDataset)[2:3] <-
+      c(as.character(buffxData[nrow(buffxData) - 0, 1]), as.character(buffxData[nrow(buffxData) -
+                                                                                  1, 1]))
+    #一旦挟まないとdatatablesのエラーが出る
+    #https://datatables.net/manual/tech-notes/1
+    tmpDataset[, 1] <- gsub("-", "-", tmpDataset[, 1])
+    tmpDataset <<- tmpDataset
+    dataset <<-
+      data.frame(ID = seq(1, nrow(tmpDataset)),
+                 tmpDataset,
+                 check.names = F)
+    
+    output$table1 <- DT::renderDataTable(
+      statisticTable,
+      rownames = F,
+      caption = "Table 1: Statistical Summary",
+      options = list(
+        autoWidth = T,
+        info = F,
+        lengthChange = F,
+        ordering = F,
+        searching = F,
+        scrollX = T,
+        paging = F
+      )
+    )
+    
+    output$table2 <- DT::renderDataTable(
+      dataset,
+      rownames = F,
+      caption = paste("Table 2: Statistical Summary for last 100 days"),
+      options = list(
+        autoWidth = T,
+        info = T,
+        lengthChange = T,
+        ordering = T,
+        searching = T,
+        scrollX = T,
+        lengthMenu = list(c(-1, 1), c("All", "1")),
+        orderClasses = TRUE,
+        order = list(list(0, "asc"))
+      )
+    )
+    
+    output$plot1 <- renderPlot({
+      par(mar = c(5, 4, 3, 3))
+      subtitle <-
+        paste(plotData[1, 1], " - ", plotData[nrow(plotData), 1])
+      if (input$charttype == "hist")
+      {
+        hist(
+          plotData[, 2],
+          breaks = 50,
+          cex.axis = 1,
+          cex.lab = 1,
+          cex.main = 1,
+          main = colnames(plotData)[2],
+          xlab = ""
+        )
+      }
+      if (input$charttype == "arima")
+      {
+        ci = c(90, 99)
+        forecastN <- 30
+        result.arima <-
+          auto.arima(plotData[, 2],
+                     ic = "aic",
+                     trace = F,
+                     stepwise = T)
+        result.forecast <-
+          forecast(result.arima, level = ci, h = forecastN)
+        plot(
+          result.forecast,
+          main = paste(
+            colnames(plotData)[2],
+            "\n",
+            subtitle,
+            " CI=",
+            ci[1],
+            ",",
+            ci[2]
+          ),
+          ylab = "",
+          panel.first = grid(
+            nx = NULL,
+            ny = NULL,
+            lty = 2,
+            equilogs = T
+          )
+        )
+      }
+      if (input$charttype == "QQ")
+      {
+        qqnorm(plotData[, 2],
+               panel.first = grid(
+                 nx = NULL,
+                 ny = NULL,
+                 lty = 2,
+                 equilogs = T
+               ))
+        qqline(plotData[, 2], col = "red")
+      }
+      if (input$charttype == "l" || input$charttype == "h")
+      {
+        plot(
+          plotData[, 1],
+          plotData[, 2],
+          type = input$charttype,
+          xlab = "",
+          ylab = colnames(plotData)[2],
+          panel.first = grid(
+            nx = NULL,
+            ny = NULL,
+            lty = 2,
+            equilogs = T
+          ),
+          xaxt = "n",
+          main = paste(colnames(plotData)[2], "\n", subtitle),
+          cex.axis = 1,
+          cex.lab = 1,
+          cex.main = 1
+        )
+        abline(h = 0, col = "red")
+        axis.Date(
+          side = 1,
+          at = plotData[, 1],
+          format = "%Y-%m-%d",
+          padj = 1,
+          cex.axis = 1
+        )
+      }
+    })
+  })
+  
+  output$remarktext <- renderUI({
+    str <- "<hr>
+    <b>Remarks</b><br>
+    <ol>
+    <li>Sample Variance = var(x)*(length(x)-1)/length(x)</li>
+    <li>Sample Standard Deviation = √Sample Variance</li>
+    <li>adf.test( x ) { tseries } , ad.test( x ) { nortest }(Normality test)</li>
+    <li><a href=\"http://www.saecanet.com\" target=\"_blank\">SaECaNet</a></li>
+    <li>Other apps <a href=\"http://webapps.saecanet.com\" target=\"_blank\">SaECaNet - Web Applications</a></li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$history <- renderUI({
+    str <- "<hr>
+    <b>History</b><br>
+    <ol>
+    <li>2016-06-15:ver.1.0.0</li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$DataDownloadTime <- renderText({
+    reactiveData()
+    paste("Data downloaded time(UTC):" ,
+          as.character(latestDataDownloadTime))
+  })
+  
+})
---
+++ shinyapps/historicalFXData/ui.R
@@ -0,0 +1,109 @@
+library(shiny)
+library(RCurl)
+script <-
+  getURL(
+    "https://raw.githubusercontent.com/am-consulting/Rscript/master/getFXhistoricalData.r",
+    ssl.verifypeer = FALSE
+  )
+eval(parse(text = script))
+
+latestDataDownloadTime <<- as.POSIXlt(Sys.time(), "GMT")
+shinyUI(fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet",
+              href = "http://fonts.googleapis.com/css?family=Roboto+Condensed"),
+    # favicon
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-144-precomposed.png",
+              sizes = "144x144"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-114-precomposed.png",
+              sizes = "114x114"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-72-precomposed.png",
+              sizes = "72x72"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-52-precomposed.png",
+              sizes = "52x52"),
+    tags$link(rel = "shortcut icon",
+              href = "http://www.saecanet.com/subfolder/ico/favicon.png"),
+    # favicon
+    tags$style("body{font-family: 'Roboto Condensed', sans-serif;}"),
+    tags$style("table{white-space: nowrap;}"),
+    tags$script(
+      '!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");'
+    )
+  ),
+  headerPanel(
+    "Historical Foreign Exchange Rate(exchange quotation,middle rate). Raw Data source:Mizuho Bank, Ltd."
+  ),
+  fluidRow(column(12,
+                  textOutput("DataDownloadTime"))),
+  
+  
+  fluidRow(column(10,
+  fluidRow(
+    column(
+      2,
+      wellPanel(
+        selectInput(
+          "currency",
+          label = "Currency",
+          colnames(origData)[-1],
+          selectize = FALSE
+        ),
+        dateRangeInput(
+          "dateRange",
+          label = "Date Range Input",
+          start = tail(origData, 1)[1, 1] - 365,
+          end = tail(origData, 1)[1, 1]
+        ),
+        radioButtons(
+          "charttype",
+          label = "Chart Type",
+          choices = list(
+            `Time series( Line )` = "l",
+            `Time series( Bar )` = "h",
+            Histogram = "hist",
+            `Q-Q plot` = "QQ" ,
+            `Arima` = "arima"
+          ),
+          selected = "l"
+        ),
+        radioButtons(
+          "datatype",
+          label = "Data Type",
+          choices = list(
+            Level = 1,
+            `1st difference` = 2  ,
+            `2nd difference` = 3
+          ),
+          selected = 1
+        )
+      )
+    ),
+    column(5,
+           wellPanel(plotOutput("plot1"))),
+    column(5,
+           DT::dataTableOutput("table1"))
+  ),
+  fluidRow(  column(
+    12,
+    DT::dataTableOutput("table2"),
+    htmlOutput("remarktext"),
+    htmlOutput("history")
+  ))
+  ) ,   
+    column(2,
+      a(
+        "@AMC2_Japan",
+        class = "twitter-timeline"
+        ,
+        href = "https://twitter.com/AMC2_Japan"
+        ,
+        "data-widget-id" = "449799943780200448",
+        width = "100%",
+        height = "2000"
+      )
+          )
+)))
