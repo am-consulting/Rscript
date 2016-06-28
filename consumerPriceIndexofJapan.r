1376b19c0f73befcdab07f450e1cb6e7abd98309
am-consulting <contact@am-consulting.co.jp>
2016-06-28 21:26:35 +0900

    文字コード追記
    文字コード追記

--- consumerPriceIndexofJapan.r
+++ consumerPriceIndexofJapan.r
@@ -1,5 +1,6 @@
 # License:GPL(version 2 or later)
 # Data Source:Statistics Bureau, Ministry of Internal Affairs and Communications
+options(download.file.method = "libcurl")
 file <-
   "http://www.e-stat.go.jp/SG1/estat/Csvdl.do?sinfid=000011288589"
 buf <-
@@ -9,7 +10,8 @@ buf <-
     skip = 0,
     stringsAsFactor = F,
     check.names = F,
-    na.strings = c("")
+    na.strings = c(""),
+    fileEncoding = "cp932"
   )
 colnames(buf) <- buf[2,]
 for (rrr in 1:nrow(buf)) {
