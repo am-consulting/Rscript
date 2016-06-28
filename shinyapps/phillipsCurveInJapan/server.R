ba121bcecedded3af3f23b6fc40477ee03981c03
am-consulting <contact@am-consulting.co.jp>
2016-06-28 21:39:38 +0900

    フィリップス曲線新規アップロード
    フィリップス曲線新規アップロード

---
+++ shinyapps/phillipsCurveInJapan/server.R
@@ -0,0 +1,449 @@
+library(shiny)
+library(tseries)
+library(forecast)
+library(DT)
+library(nortest)
+library(lubridate)
+library(ggplot2)
+shinyServer(function(input, output, session)
+{
+  funPC <- function(sss) {
+    tmp <-
+      bufData[, c(1, which(objData == colnames(bufData)))]
+    tmp <- na.omit(tmp)
+    if (nrow(tmp) != 0) {
+      tmp <- subset(tmp, startData <= index(tmp) & index(tmp) <= endData)
+      buf <- colnames(tmp)
+      if (input$datatype == 2)
+        #1st diff
+      {
+        tmp <-
+          data.frame(tmp[-1, 1], diff(tmp[, 2], lag = 1, differences = 1),
+                     check.names = F)
+      }
+      if (input$datatype == 3)
+        #2nd diff
+      {
+        tmp <-
+          data.frame(tmp[-c(1, 2), 1], diff(tmp[, 2], lag = 1, differences = 2),
+                     check.names = F)
+      }
+      colnames(tmp) <- buf
+      plotData <- tmp
+      
+      assign(paste("plotData", sss, sep = ""), plotData, envir = .GlobalEnv)
+      
+      subtitle <-
+        paste(format(plotData[1, 1], "%Y-%m"),
+              " - ",
+              format(plotData[nrow(plotData), 1], "%Y-%m"))
+      
+      x <- plotData[, 2]
+      SV <- var(x) * (length(x) - 1) / length(x) #sample variance
+      UV <- var(x)#unbiased variance
+      SSD <- sqrt(SV)#sample standard deviation
+      minData <- tail(subset(plotData, min(x) == x), 1)
+      maxData <- tail(subset(plotData, max(x) == x), 1)
+      statistic <-
+        c(
+          "n",
+          "mean( x )",
+          "median( x )",
+          "min( x )",
+          "max( x )",
+          "sd( x )",
+          "Sample Standard Deviation",
+          "var( x )",
+          "Sample Variance",
+          "adf.test( x )",
+          "ad.test( x )",
+          "Variation Coefficient"
+        )
+      result <-
+        c(length(x), round(
+          c(
+            mean(x),
+            median(x),
+            minData[, 2],
+            maxData[, 2],
+            sd(x),
+            SSD  ,
+            UV ,
+            SV ,
+            adf.test(x)$p.value,
+            ad.test(x)$p.value,
+            SSD / mean(x) * 100
+          ),
+          3
+        ))
+      remark <-
+        c(
+          "-",
+          "-",
+          "-",
+          paste(format(minData[, 1], "%Y-%m"), ""),
+          paste(format(maxData[, 1], "%Y-%m"), ""),
+          "-",
+          "-",
+          "-",
+          "-",
+          "p-value",
+          paste("p-value:", ad.test(x)$method),
+          "%"
+        )
+      
+      assign(
+        paste("statisticTable", sss, sep = ""),
+        data.frame(
+          Statistic = statistic,
+          Result = result,
+          Remark = remark,
+          stringsAsFactors = F,
+          check.names = F
+        ),
+        envir = .GlobalEnv
+      )
+      
+      bufTSData <-
+        data.frame(c(NA, diff(plotData[, -1])))
+      colnames(bufTSData) <- "Changes from previous data(pp)"
+      #      bufTSData[, 1] <- bufTSData[, 1]
+      bufplotData <- plotData
+      bufplotData[, 1] <- format(bufplotData[, 1], "%Y-%m")
+      assign(
+        paste("timeseriesData", sss, sep = ""),
+        data.frame(bufplotData, bufTSData, check.names = F),
+        envir = .GlobalEnv
+      )
+      
+      assign(paste("plot", sss, sep = ""), renderPlot({
+        par(mar = c(5, 4, 3, 3), family = "Noto Sans Japanese")
+        if (nrow(na.omit(plotData)) != 0) {
+          if (input$charttype == "hist")
+          {
+            hist(
+              plotData[, 2],
+              breaks = 50,
+              cex.axis = 1,
+              cex.lab = 1,
+              cex.main = 1,
+              main = paste(colnames(plotData)[2], "\n", subtitle),
+              xlab = ""
+            )
+          }
+          if (input$charttype == "arima")
+          {
+            ci = c(90, 99)
+            forecastN <- 30
+            result.arima <-
+              auto.arima(
+                plotData[, 2],
+                ic = "aic",
+                trace = F,
+                stepwise = T
+              )
+            result.forecast <-
+              forecast(result.arima, level = ci, h = forecastN)
+            plot(
+              result.forecast,
+              main = paste(
+                colnames(plotData)[2],
+                "\n",
+                subtitle,
+                " CI=",
+                ci[1],
+                ",",
+                ci[2]
+              ),
+              ylab = "",
+              panel.first = grid(
+                nx = NULL,
+                ny = NULL,
+                lty = 2,
+                equilogs = T
+              )
+            )
+          }
+          if (input$charttype == "QQ")
+          {
+            qqnorm(plotData[, 2],
+                   panel.first = grid(
+                     nx = NULL,
+                     ny = NULL,
+                     lty = 2,
+                     equilogs = T
+                   ))
+            qqline(plotData[, 2], col = "red")
+          }
+          if (input$charttype == "l" || input$charttype == "h")
+          {
+            plot(
+              plotData[, 1],
+              plotData[, 2],
+              type = input$charttype,
+              xlab = "",
+              ylab = colnames(plotData)[2],
+              panel.first = grid(
+                nx = NULL,
+                ny = NULL,
+                lty = 2,
+                equilogs = T
+              ),
+              xaxt = "n",
+              main = paste(colnames(plotData)[2], "\n", subtitle),
+              cex.axis = 1,
+              cex.lab = 1,
+              cex.main = 1
+            )
+            abline(h = 0, col = "red")
+            axis.Date(
+              side = 1,
+              at = plotData[, 1],
+              format = "%Y-%m",
+              padj = 1,
+              cex.axis = 1
+            )
+          }
+        }
+      })
+      , envir = .GlobalEnv)
+    } else{
+      assign(paste("plot", sss, sep = ""), renderPlot({
+        plot.new()
+      }), envir = .GlobalEnv)
+      
+      assign(paste("statisticTable", sss, sep = ""),
+             data.frame(),
+             envir = .GlobalEnv)
+      
+      assign(paste("timeseriesData", sss, sep = ""),
+             data.frame(),
+             envir = .GlobalEnv)
+      
+    }
+    assign(
+      paste("stDT", sss, sep = ""),
+      DT::renderDataTable(
+        get(paste("statisticTable", sss, sep = "")),
+        rownames = F,
+        caption = paste(
+          "Table ",
+          sss + 1,
+          colnames(plotData)[2],
+          ": Statistical Summary.",
+          subtitle
+        ),
+        options = list(
+          autoWidth = T,
+          info = F,
+          lengthChange = F,
+          ordering = F,
+          searching = F,
+          scrollX = T,
+          paging = F
+        )
+      ),
+      envir = .GlobalEnv
+    )
+    
+    assign(
+      paste("tsDT", sss, sep = ""),
+      DT::renderDataTable(
+        get(paste("timeseriesData", sss, sep = "")),
+        rownames = F,
+        caption = paste("Table ", sss + 2, ": Time Series Data.", subtitle),
+        options = list(
+          autoWidth = T,
+          info = T,
+          lengthChange = T,
+          ordering = T,
+          searching = T,
+          scrollX = T,
+          lengthMenu = list(c(10, -1, 1), c("10", "All", "1")),
+          orderClasses = TRUE,
+          order = list(list(0, "desc"))
+        )
+      ),
+      envir = .GlobalEnv
+    )
+  }
+  
+  reactiveData <- reactive({
+    sss <- 0
+    bufData <<- laborforcesurvey
+    objData <<- input$lfs
+    startData <<- input$lfsRange[1]
+    endData <<- input$lfsRange[2]
+    funPC(sss)
+    output$plot01 <- get(paste("plot", sss, sep = ""))
+    output$statisticTable01 <- get(paste("stDT", sss, sep = ""))
+    output$timeseriesTable01 <- get(paste("tsDT", sss, sep = ""))
+    
+    sss <- 2
+    bufData <<- consumerpriceindex
+    objData <<- input$cpi
+    startData <<- input$cpiRange[1]
+    endData <<- input$cpiRange[2]
+    funPC(sss)
+    output$plot02 <- get(paste("plot", sss, sep = ""))
+    output$statisticTable02 <- get(paste("stDT", sss, sep = ""))
+    output$timeseriesTable02 <- get(paste("tsDT", sss, sep = ""))
+    
+    mergeData <- na.omit(merge(plotData0, plotData2, by = "Date"))
+    
+    output$plotmerge <- renderPlot({
+      par(mar = c(5, 5, 3, 5), family = "Noto Sans Japanese")
+      plot(
+        mergeData[, 1],
+        mergeData[, 2],
+        type = "o",
+        col = "blue",
+        xlab = "" ,
+        ylab = "" ,
+        panel.first = grid(
+          nx = NULL,
+          ny = NULL,
+          lty = 2,
+          equilogs = T
+        ) ,
+        xaxt = "n",
+        main = paste(colnames(mergeData)[2], "×", colnames(mergeData)[3]),
+        cex.axis = 1,
+        cex.lab = 1,
+        cex.main = 1
+      )
+      par(new = T)
+      plot(
+        mergeData[, 1],
+        mergeData[, 3],
+        col = "red",
+        type = "o",
+        xlab = "" ,
+        ylab = "" ,
+        panel.first = grid(
+          nx = NULL,
+          ny = NULL,
+          lty = 2,
+          equilogs = T
+        ) ,
+        xaxt = "n",
+        yaxt = "n",
+        cex.axis = 1,
+        cex.lab = 1,
+        cex.main = 1
+      )
+      axis(4, cex.axis = 1, cex.lab = 1)
+      legend(
+        "topleft",
+        col = c("blue", "red"),
+        pch = 1,
+        legend = c(colnames(mergeData)[2], colnames(mergeData)[3]),
+        cex = 1
+      )
+      mtext(
+        colnames(mergeData)[2],
+        side = 2,
+        line = 3.2,
+        cex = 1
+      )
+      mtext(
+        colnames(mergeData)[3],
+        side = 4,
+        line = 3.2,
+        cex = 1
+      )
+      axis.Date(
+        side = 1,
+        at = mergeData[, 1],
+        format = "%Y-%m",
+        padj = 1,
+        cex.axis = 1
+      )
+    })
+    
+    output$PhillipsCurve <- renderPlot({
+      dataSet <- mergeData
+      g <- ggplot()
+      g <-
+        g + geom_point(data = dataSet,
+                       aes(x = dataSet[, 2], y = dataSet[, 3]),
+                       size = 1)
+      g <-
+        g + ggtitle(paste(colnames(dataSet)[2], "×", colnames(dataSet)[3]))
+      g <-
+        g + xlab(colnames(dataSet)[2]) + ylab(colnames(dataSet)[3])
+      g <- g + theme(plot.title = element_text(size = 17))
+      g <- g + theme(axis.title.x = element_text(size = 17))
+      g <- g + theme(axis.title.y = element_text(size = 17))
+      g <-
+        g + theme(axis.text.x = element_text(size = 17,
+                                             angle = 0))
+      g <-
+        g + theme(axis.text.y = element_text(size = 17,
+                                             angle = 90))
+      g <-
+        g + geom_text(
+          data = dataSet,
+          aes(
+            x = dataSet[, 2],
+            y = dataSet[, 3],
+            label = format(dataSet[, 1], "%Y-%m")
+          ),
+          size = 4,
+          vjust = -0.5,
+          hjust = 0.5
+        )
+      g <-
+        g + geom_path(
+          data = dataSet,
+          aes(x = dataSet[, 2], y = dataSet[, 3]),
+          col = "blue",
+          size = 0.5
+        )
+      print(g)
+    })
+  })
+  
+  output$remarktext <- renderUI({
+    str <- "<hr>
+    <b>Remarks</b><br>
+    <ol>
+    <li>Unit: 10 thousand people , percent</li>
+    <li>Sample Variance = var(x)*(length(x)-1)/length(x)</li>
+    <li>Sample Standard Deviation = √Sample Variance</li>
+    <li>adf.test( x ) { tseries } , ad.test( x ) { nortest }(Normality test)</li>
+    <li>Variation Coefficient(VC,%)=(Sample Standard Deviation/Mean)*100</li>
+    <li><a href=\"http://github.hubspot.com/pace/docs/welcome/\" target=\"_blank\">PACE - http://github.hubspot.com/pace/docs/welcome/</a></li>
+    <li><a href=\"http://www.saecanet.com\" target=\"_blank\">SaECaNet</a></li>
+    <li>Other apps <a href=\"http://webapps.saecanet.com\" target=\"_blank\">SaECaNet - Web Applications</a></li>
+    <li><a href=\"http://am-consulting.co.jp\" target=\"_blank\">Asset Management Consulting Corporation / アセット･マネジメント･コンサルティング株式会社</a></li>
+    <li><a href=\"http://www.saecanet.com/subfolder/disclaimer.html\" target=\"_blank\">Disclaimer</a></li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$history <- renderUI({
+    str <- "<hr>
+    <b>History</b><br>
+    <ol>
+    <li>2016-06-28:ver.1.0.0</li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$gitcode <- renderUI({
+    str <- "<hr>
+    <b>Code</b><br>
+    <ol>
+    <li><a href=\"https://github.com/am-consulting/Rscript/tree/master/shinyapps/phillipsCurveInJapan\" target=\"_blank\">Move to GitHub</a></li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$DataDownloadTime <- renderText({
+    reactiveData()
+    paste("Data downloaded time(UTC):" ,
+          as.character(latestDataDownloadTime))
+  })
+  
+})
---
+++ shinyapps/phillipsCurveInJapan/ui.R
@@ -0,0 +1,158 @@
+library(shiny)
+library(RCurl)
+script <-
+  getURL(
+    "https://raw.githubusercontent.com/am-consulting/Rscript/master/laborForceSurveyofJapan.r",
+    ssl.verifypeer = FALSE
+  )
+eval(parse(text = script))
+laborforcesurvey <<- origData
+
+script <-
+  getURL(
+    "https://raw.githubusercontent.com/am-consulting/Rscript/master/consumerPriceIndexofJapan.r",
+    ssl.verifypeer = FALSE
+  )
+eval(parse(text = script))
+consumerpriceindex <<- origData0
+
+latestDataDownloadTime <<- as.POSIXlt(Sys.time(), "GMT")
+shinyUI(fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet",
+              href = "http://fonts.googleapis.com/css?family=Roboto+Condensed"),
+    tags$link(rel = "stylesheet",
+              href = "http://archive.am-consulting.co.jp/paceCSS.css"),
+    tags$script(src = "http://archive.am-consulting.co.jp/pace.min.js"),
+    # favicon
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-144-precomposed.png",
+              sizes = "144x144"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-114-precomposed.png",
+              sizes = "114x114"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-72-precomposed.png",
+              sizes = "72x72"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-52-precomposed.png",
+              sizes = "52x52"),
+    tags$link(rel = "shortcut icon",
+              href = "http://www.saecanet.com/subfolder/ico/favicon.png"),
+    # favicon
+    tags$style("body{font-family: 'Roboto Condensed', sans-serif;}"),
+    tags$style("table{white-space: nowrap;}"),
+    tags$script(
+      '!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");'
+    )
+  ),
+  headerPanel(
+    "Phillips Curve in Japan. Labor Force Survey & Consumer Price Index. Raw Data source:Statistics Bureau, Ministry of Internal Affairs and Communications"
+  ),
+  fluidRow(column(12,
+                  textOutput("DataDownloadTime"))),
+  fluidRow(column(10,
+                  fluidRow(
+                    column(
+                      2,
+                      wellPanel(
+                        selectInput(
+                          "lfs",
+                          label = "Labor Force Survey",
+                          colnames(laborforcesurvey)[-1],
+                          selectize = FALSE
+                        ),
+                        sliderInput(
+                          "lfsRange",
+                          label = "Labor Force Survey",
+                          min = 1,
+                          max = nrow(laborforcesurvey),
+                          value = c(1, nrow(laborforcesurvey))
+                        ),
+                        selectInput(
+                          "cpi",
+                          label = "Consumer Price Index",
+                          colnames(consumerpriceindex)[-1],
+                          selectize = FALSE
+                        ),
+                        sliderInput(
+                          "cpiRange",
+                          label = "Consumer Price Index",
+                          min = 1,
+                          max = nrow(consumerpriceindex),
+                          value = c(1, nrow(consumerpriceindex))
+                        ),
+                        radioButtons(
+                          "charttype",
+                          label = "Chart Type",
+                          choices = list(
+                            `Time series( Line )` = "l",
+                            `Time series( Bar )` = "h",
+                            Histogram = "hist",
+                            `Q-Q plot` = "QQ" ,
+                            `Arima` = "arima"
+                          ),
+                          selected = "l"
+                        ),
+                        radioButtons(
+                          "datatype",
+                          label = "Data Type",
+                          choices = list(
+                            Level = 1,
+                            `1st difference` = 2  ,
+                            `2nd difference` = 3
+                          ),
+                          selected = 1
+                        )
+                      )
+                    ),
+                    column(
+                      10,
+                      fluidRow(column(6, wellPanel(
+                        plotOutput("plot01", height = "400px")
+                      )),
+                      column(6, wellPanel(
+                        plotOutput("plot02", height = "400px")
+                      ))),
+                      fluidRow(column(12, wellPanel(
+                        plotOutput("plotmerge", height = "500px")
+                      ))),
+                      fluidRow(column(12, wellPanel(
+                        plotOutput("PhillipsCurve", height = "500px")
+                      ))),
+                      tags$hr(),
+                      fluidRow(
+                        column(6,
+                               DT::dataTableOutput("statisticTable01")),
+                        column(6,
+                               DT::dataTableOutput("statisticTable02"))
+                      ),
+                      tags$hr(),
+                      fluidRow(
+                        column(6,
+                               DT::dataTableOutput("timeseriesTable01")),
+                        column(6,
+                               DT::dataTableOutput("timeseriesTable02"))
+                      ),
+                      fluidRow(column(
+                        12,
+                        htmlOutput("remarktext"),
+                        htmlOutput("history"),
+                        htmlOutput("gitcode")
+                      ))
+                    )
+                  )),
+           column(
+             2,
+             a(
+               "@AMC2_Japan",
+               class = "twitter-timeline"
+               ,
+               href = "https://twitter.com/AMC2_Japan"
+               ,
+               "data-widget-id" = "449799943780200448",
+               width = "100%",
+               height = "3000"
+             )
+           ))
+))
