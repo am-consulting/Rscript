8ef85df71946c5339e2b1cc8c3ee9489d4687c94
am-consulting <contact@am-consulting.co.jp>
2016-06-28 13:38:28 +0900

    労働力調査および消費者物価指数インポートコード新規アップロード
    労働力調査および消費者物価指数インポートコード新規アップロード

---
+++ consumerPriceIndexofJapan.r
@@ -0,0 +1,32 @@
+# License:GPL(version 2 or later)
+# Data Source:Statistics Bureau, Ministry of Internal Affairs and Communications
+file <-
+  "http://www.e-stat.go.jp/SG1/estat/Csvdl.do?sinfid=000011288589"
+buf <-
+  read.csv(
+    file,
+    header = F,
+    skip = 0,
+    stringsAsFactor = F,
+    check.names = F,
+    na.strings = c("")
+  )
+colnames(buf) <- buf[2,]
+for (rrr in 1:nrow(buf)) {
+  tmp <- as.numeric(substr(buf[rrr, 1], 1, 6))
+  if (!is.na(tmp)) {
+    break
+  }
+}
+startDate <-
+  as.Date(paste(substr(tmp, 1, 4), "-", substr(tmp, 5, 6), "-1", sep = ""))
+origData <- buf[-(1:(rrr - 1)), 2:ncol(buf)]
+apply(origData, 2, function(x) {
+  as.numeric(x)
+})
+origData <<-
+  data.frame(
+    Date = seq(startDate, length.out = nrow(origData), by = "months"),
+    origData,
+    check.names = F
+  )
---
+++ laborForceSurveyofJapan.r
@@ -0,0 +1,50 @@
+# License:GPL(version 2 or later)
+# Data Source:Statistics Bureau, Ministry of Internal Affairs and Communications
+library(quantmod)
+library(gdata)
+options(download.file.method = "libcurl")
+perl <- gdata:::findPerl("perl")
+dataURL <-
+  c("http://www.stat.go.jp/data/roudou/longtime/zuhyou/lt01-a10.xls",1)
+for (iii in seq(1, length(dataURL), by = 2)) {
+  buf <-
+    read.xls(
+      dataURL[iii],
+      perl = perl,
+      check.names = F,
+      header = F,
+      stringsAsFactors = F,
+      sheet = dataURL[iii + 1],
+      fileEncoding = "utf-8"
+    )
+  gc()
+  gc()
+}
+for (rrr in 1:nrow(buf)) {
+  tmp <- as.numeric(substr(buf[rrr, 1], 1, 4))
+  if (!is.na(tmp)) {
+    break
+  }
+}
+year <- tmp
+rrr <- rrr - 4 #caution
+bufdataSet <- buf[rrr:nrow(buf), 5:ncol(buf)] #caution
+for (ccc in 1:ncol(bufdataSet)) {
+  if (bufdataSet[1, ccc] != "") {
+    tmpcolname <- bufdataSet[1, ccc]
+  }
+  bufdataSet[3, ccc] <-
+    paste(tmpcolname, "-",  bufdataSet[3, ccc], sep = "")
+}
+colnames(bufdataSet) <- bufdataSet[3, ]
+bufdataSet <- bufdataSet[-(1:3), ]
+startDate <- as.Date(paste(year, "-1-1", sep = ""))
+origData <-
+  data.frame(Date = as.Date(seq(
+    startDate, length.out = nrow(bufdataSet), by = "months"
+  )), bufdataSet, check.names = F)
+origData[, -1] <-
+  apply(origData[, -1], 2, function(x) {
+    as.numeric(gsub("\\(|\\)|<|>", "", x)) #caution
+  })
+origData <<- origData
