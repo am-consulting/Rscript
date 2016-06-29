599cb70ae21e4f59f80c6afb5fe6d945aa528190
am-consulting <contact@am-consulting.co.jp>
2016-06-29 15:38:40 +0900

    oandaからメジャー通貨のみインポートコード新規アップロード
    oandaからメジャー通貨のみインポートコード新規アップロード

---
+++ importFXRateBygetFX_major.r
@@ -0,0 +1,45 @@
+# License:GPL(version 2 or later)
+# Reference https://cran.r-project.org/web/packages/quantmod/quantmod.pdf
+library(quantmod)
+options(download.file.method="libcurl")
+currencyList <-
+  c(
+    "USD/JPY",
+    "USD/EUR",
+    "USD/GBP"
+  )
+currencyName <<-data.frame(
+  Symbols=c(
+    "USD",
+    "JPY",
+    "EUR",
+    "GBP"
+    ),
+  Currency=c(
+    "U.S. Dollar",
+    "Japanese Yen",
+    "Euro",
+    "British Pound"
+  )
+)  
+for (iii in 1:length(currencyList)) {
+  Sys.sleep(2) #avoid to overload  
+  buf <-
+    getSymbols(currencyList[iii], src = "oanda", auto.assign = F, from = Sys.Date()-365*5, to = Sys.Date())
+  if (iii == 1) {
+    origData <- buf
+  } else{
+    origData <- merge(origData, buf, all = T)
+  }
+  gc();gc()
+}
+colnames(origData) <-
+  gsub("USD.", "USD/", colnames(origData))
+origData <-
+  data.frame(
+    Date = index(origData),
+    origData,
+    row.names = NULL,
+    check.names = F
+  )
+origData <<- origData
