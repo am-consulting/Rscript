bf55d7b3380869cd162d0ea78ee0cc1bdf43aac3
am-consulting <contact@am-consulting.co.jp>
2016-07-04 14:57:53 +0900

    財務省貿易統計新規アップロード
    財務省貿易統計新規アップロード

---
+++ shinyapps/tradeStatisticsOfJapan/server.R
@@ -0,0 +1,382 @@
+packageList <-
+  c("shiny", "tseries", "forecast", "DT", "nortest", "lubridate")
+lapply(packageList, require, character.only = T)
+shinyServer(function(input, output, session)
+{
+  fun <- function(sss) {
+    tmp0 <- get(paste("origData", sss, sep = ""))
+    tmp <-
+      tmp0[, c(1, which(obj == colnames(tmp0)))]
+    tmp <- na.omit(tmp)
+    
+    assign(paste("dataTitle", sss, sep = ""),
+           colnames(tmp)[2],
+           envir = .GlobalEnv)
+    assign(paste("title", sss, sep = ""), renderText({
+      get(paste("dataTitle", sss, sep = ""))
+    }), envir = .GlobalEnv)
+    
+    if (nrow(tmp) != 0) {
+      switch (
+        sss,
+        dataRange <- input$dataRange1,
+        dataRange <- input$dataRange2,
+        dataRange <- input$dataRange3,
+        dataRange <- input$dataRange4
+      )
+      tmp <-
+        subset(tmp,
+               dataRange[1] <= index(tmp) &
+                 index(tmp) <= dataRange[2])
+      buf <- colnames(tmp)
+      if (input$datatype == 2)
+        #1st diff
+      {
+        tmp <-
+          data.frame(tmp[-1, 1], diff(tmp[, 2], lag = 1, differences = 1),
+                     check.names = F)
+      }
+      if (input$datatype == 3)
+        #2nd diff
+      {
+        tmp <-
+          data.frame(tmp[-c(1, 2), 1], diff(tmp[, 2], lag = 1, differences = 2),
+                     check.names = F)
+      }
+      colnames(tmp) <- buf
+      plotData <- tmp
+      subtitle <-
+        paste(format(plotData[1, 1], "%Y-%m"),
+              " - ",
+              format(plotData[nrow(plotData), 1], "%Y-%m"))
+      
+      x <- plotData[, 2]
+      SV <- var(x) * (length(x) - 1) / length(x) # sample variance
+      UV <- var(x) # unbiased variance
+      SSD <- sqrt(SV) # sample standard deviation
+      minData <- tail(subset(plotData, min(x) == x), 1)
+      maxData <- tail(subset(plotData, max(x) == x), 1)
+      statistic <-
+        c(
+          "n",
+          "mean( x )",
+          "median( x )",
+          "min( x )",
+          "max( x )",
+          "sd( x )",
+          "Sample Standard Deviation",
+          "var( x )",
+          "Sample Variance",
+          "adf.test( x )",
+          "ad.test( x )",
+          "Variation Coefficient"
+        )
+      result <-
+        c(length(x), round(
+          c(
+            mean(x),
+            median(x),
+            minData[, 2],
+            maxData[, 2],
+            sd(x),
+            SSD  ,
+            UV ,
+            SV ,
+            adf.test(x)$p.value,
+            ad.test(x)$p.value,
+            SSD / mean(x) * 100
+          ),
+          3
+        ))
+      remark <-
+        c(
+          "-",
+          "-",
+          "-",
+          paste(format(minData[, 1], "%Y-%m"), ""),
+          paste(format(maxData[, 1], "%Y-%m"), ""),
+          "-",
+          "-",
+          "-",
+          "-",
+          "p-value",
+          paste("p-value:", ad.test(x)$method),
+          "%"
+        )
+      
+      assign(
+        paste("statisticTable", sss, sep = ""),
+        data.frame(
+          Statistic = statistic,
+          Result = result,
+          Remark = remark,
+          stringsAsFactors = F,
+          check.names = F
+        ),
+        envir = .GlobalEnv
+      )
+      
+      bufTSData <-
+        data.frame(c(NA, round(((plotData[-1, -1] / plotData[-nrow(plotData), -1]) - 1
+        ) * 100, 2)))
+      colnames(bufTSData) <- "Changes from previous data(%)"
+      bufTSData[, 1] <- bufTSData[, 1]
+      bufplotData <- plotData
+      bufplotData[, 1] <- format(bufplotData[, 1], "%Y-%m")
+      assign(
+        paste("timeseriesData", sss, sep = ""),
+        data.frame(bufplotData, bufTSData, check.names = F),
+        envir = .GlobalEnv
+      )
+      
+      assign(paste("plot", sss, sep = ""), renderPlot({
+        par(mar = c(5, 5, 3, 5), family = "Noto Sans Japanese")
+        if (input$charttype == "hist")
+        {
+          hist(
+            plotData[, 2],
+            breaks = 50,
+            cex.axis = 1,
+            cex.lab = 1,
+            cex.main = 1,
+            main = paste(colnames(plotData)[2], "\n", subtitle),
+            xlab = ""
+          )
+        }
+        if (input$charttype == "arima")
+        {
+          ci = c(90, 99)
+          forecastN <- 30
+          result.arima <-
+            auto.arima(
+              plotData[, 2],
+              ic = "aic",
+              trace = F,
+              stepwise = T
+            )
+          result.forecast <-
+            forecast(result.arima, level = ci, h = forecastN)
+          plot(
+            result.forecast,
+            main = paste(
+              colnames(plotData)[2],
+              "\n",
+              subtitle,
+              " CI=",
+              ci[1],
+              ",",
+              ci[2]
+            ),
+            ylab = "",
+            panel.first = grid(
+              nx = NULL,
+              ny = NULL,
+              lty = 2,
+              equilogs = T
+            )
+          )
+        }
+        if (input$charttype == "QQ")
+        {
+          qqnorm(plotData[, 2],
+                 panel.first = grid(
+                   nx = NULL,
+                   ny = NULL,
+                   lty = 2,
+                   equilogs = T
+                 ))
+          qqline(plotData[, 2], col = "red")
+        }
+        if (input$charttype == "l" || input$charttype == "h")
+        {
+          plot(
+            plotData[, 1],
+            plotData[, 2],
+            type = input$charttype,
+            xlab = "",
+            ylab = colnames(plotData)[2],
+            panel.first = grid(
+              nx = NULL,
+              ny = NULL,
+              lty = 2,
+              equilogs = T
+            ),
+            xaxt = "n",
+            main = paste(colnames(plotData)[2], "\n", subtitle),
+            cex.axis = 1,
+            cex.lab = 1,
+            cex.main = 1
+          )
+          abline(h = 0, col = "red")
+          axis.Date(
+            side = 1,
+            at = plotData[, 1],
+            format = "%Y-%m",
+            padj = 1,
+            cex.axis = 1
+          )
+        }
+      })
+      , envir = .GlobalEnv)
+    } else{
+      assign(paste("plot", sss, sep = ""), renderPlot({
+        plot.new()
+      }), envir = .GlobalEnv)
+      
+      assign(paste("statisticTable", sss, sep = ""),
+             data.frame(),
+             envir = .GlobalEnv)
+      
+      assign(paste("timeseriesData", sss, sep = ""),
+             data.frame(),
+             envir = .GlobalEnv)
+      
+      subtitle <- ""
+    }
+    
+    assign(
+      paste("stDT", sss, sep = ""),
+      DT::renderDataTable(
+        get(paste("statisticTable", sss, sep = "")),
+        rownames = F,
+        caption = paste("Table: Statistical Summary.", subtitle),
+        options = list(
+          autoWidth = T,
+          info = F,
+          lengthChange = F,
+          ordering = F,
+          searching = F,
+          scrollX = T,
+          paging = F
+        )
+      ),
+      envir = .GlobalEnv
+    )
+    
+    assign(
+      paste("tsDT", sss, sep = ""),
+      DT::renderDataTable(
+        get(paste("timeseriesData", sss, sep = "")),
+        rownames = F,
+        caption = paste("Table: Time Series Data.", subtitle),
+        options = list(
+          autoWidth = T,
+          info = T,
+          lengthChange = T,
+          ordering = T,
+          searching = T,
+          scrollX = T,
+          lengthMenu = list(c(10, -1, 1), c("10", "All", "1")),
+          orderClasses = TRUE,
+          order = list(list(0, "desc"))
+        )
+      ),
+      envir = .GlobalEnv
+    )
+  }
+  
+  reactiveData <- reactive({
+    sss <- 1
+    obj <<- input$objective1
+    fun(sss)
+    output$plot01 <- get(paste("plot", sss, sep = ""))
+    output$statisticTable01 <- get(paste("stDT", sss, sep = ""))
+    output$timeseriesTable01 <- get(paste("tsDT", sss, sep = ""))
+    output$title01 <-
+      output$title001 <-
+      output$title0001 <- get(paste("title", sss, sep = ""))
+    
+    sss <- 2
+    obj <<- input$objective2
+    fun(sss)
+    output$plot02 <- get(paste("plot", sss, sep = ""))
+    output$statisticTable02 <- get(paste("stDT", sss, sep = ""))
+    output$timeseriesTable02 <- get(paste("tsDT", sss, sep = ""))
+    output$title02 <-
+      output$title002 <-
+      output$title0002 <- get(paste("title", sss, sep = ""))
+    
+    sss <- 3
+    obj <<- input$objective3
+    fun(sss)
+    output$plot03 <- get(paste("plot", sss, sep = ""))
+    output$statisticTable03 <- get(paste("stDT", sss, sep = ""))
+    output$timeseriesTable03 <- get(paste("tsDT", sss, sep = ""))
+    output$title03 <-
+      output$title003 <-
+      output$title0003 <- get(paste("title", sss, sep = ""))
+    
+    sss <- 4
+    obj <<- input$objective4
+    fun(sss)
+    output$plot04 <- get(paste("plot", sss, sep = ""))
+    output$statisticTable04 <- get(paste("stDT", sss, sep = ""))
+    output$timeseriesTable04 <- get(paste("tsDT", sss, sep = ""))
+    output$title04 <-
+      output$title004 <-
+      output$title0004 <- get(paste("title", sss, sep = ""))
+    
+    
+    allData <- origData1
+    allData[, 1] <- format(allData[, 1], "%Y-%m")
+    output$allData <- DT::renderDataTable(
+      origData1,
+      rownames = F,
+      caption = paste("Table: Time Series Data."),
+      options = list(
+        autoWidth = T,
+        info = T,
+        lengthChange = T,
+        ordering = T,
+        searching = T,
+        scrollX = T,
+        lengthMenu = list(c(10, -1, 1), c("10", "All", "1")),
+        orderClasses = TRUE,
+        order = list(list(0, "desc"))
+      )
+    )
+    
+  })
+  
+  output$remarktext <- renderUI({
+    str <- "<hr>
+    <b>Remarks</b><br>
+    <ol>
+    <li>Sample Variance = var(x)*(length(x)-1)/length(x)</li>
+    <li>Sample Standard Deviation = √Sample Variance</li>
+    <li>adf.test( x ) { tseries } , ad.test( x ) { nortest }(Normality test)</li>
+    <li>Variation Coefficient(VC,%)=(Sample Standard Deviation/Mean)*100</li>
+    <li><a href=\"http://github.hubspot.com/pace/docs/welcome/\" target=\"_blank\">PACE - http://github.hubspot.com/pace/docs/welcome/</a></li>
+    <li><a href=\"http://www.saecanet.com\" target=\"_blank\">SaECaNet</a></li>
+    <li>Other apps <a href=\"http://webapps.saecanet.com\" target=\"_blank\">SaECaNet - Web Applications</a></li>
+    <li><a href=\"http://am-consulting.co.jp\" target=\"_blank\">Asset Management Consulting Corporation / アセット･マネジメント･コンサルティング株式会社</a></li>
+    <li><a href=\"http://www.saecanet.com/subfolder/disclaimer.html\" target=\"_blank\">Disclaimer</a></li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$history <- renderUI({
+    str <- "<hr>
+    <b>History</b><br>
+    <ol>
+    <li>2016-07-04:ver.1.0.0</li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$gitcode <- renderUI({
+    str <- "<hr>
+    <b>Code</b><br>
+    <ol>
+    <li><a href=\"https://github.com/am-consulting/Rscript/tree/master/shinyapps/tradeStatisticsOfJapan\" target=\"_blank\">Move to GitHub</a></li>
+    </ol>"
+    HTML(str)
+  })
+  
+  output$DataDownloadTime <- renderText({
+    reactiveData()
+    paste("Data downloaded time(UTC):" ,
+          as.character(latestDataDownloadTime))
+  })
+  
+})
---
+++ shinyapps/tradeStatisticsOfJapan/ui.R
@@ -0,0 +1,246 @@
+library(shiny)
+library(RCurl)
+script <-
+  getURL(
+    "https://raw.githubusercontent.com/am-consulting/Rscript/master/importCustomExportImportData.r",
+    ssl.verifypeer = FALSE
+  )
+eval(parse(text = script))
+
+latestDataDownloadTime <<- as.POSIXlt(Sys.time(), "GMT")
+shinyUI(fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet",
+              href = "http://fonts.googleapis.com/css?family=Roboto+Condensed"),
+    tags$link(rel = "stylesheet",
+              href = "http://archive.am-consulting.co.jp/paceCSS.css"),
+    tags$script(src = "http://archive.am-consulting.co.jp/pace.min.js"),
+    # favicon
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-144-precomposed.png",
+              sizes = "144x144"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-114-precomposed.png",
+              sizes = "114x114"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-72-precomposed.png",
+              sizes = "72x72"),
+    tags$link(rel = "apple-touch-icon-precomposed",
+              href = "http://www.saecanet.com/subfolder/ico/apple-touch-icon-52-precomposed.png",
+              sizes = "52x52"),
+    tags$link(rel = "shortcut icon",
+              href = "http://www.saecanet.com/subfolder/ico/favicon.png"),
+    # favicon
+    tags$style("body{font-family: 'Roboto Condensed', sans-serif;}"),
+    tags$style("table{white-space: nowrap;}"),
+    tags$script(
+      '!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");'
+    )
+  ),
+  headerPanel(
+    "Trade Statistics of Japan. Raw Data source:Ministry of Finance Japan"
+  ),
+  fluidRow(column(12,
+                  textOutput("DataDownloadTime"))),
+  fluidRow(column(10,
+                  fluidRow(
+                    column(
+                      2,
+                      wellPanel(
+                        selectInput(
+                          "objective1",
+                          label = "By Region:World",
+                          colnames(origData1)[-1],
+                          selectize = FALSE
+                        ),
+                        sliderInput(
+                          "dataRange1",
+                          label = "Data Range",
+                          min = 1,
+                          max = nrow(origData1),
+                          value = c(1, nrow(origData1))
+                        ),
+                        tags$hr(),
+                        selectInput(
+                          "objective2",
+                          label = " By product category:Export",
+                          colnames(origData2)[-1],
+                          selectize = FALSE
+                        ),
+                        sliderInput(
+                          "dataRange2",
+                          label = "Data Range",
+                          min = 1,
+                          max = nrow(origData2),
+                          value = c(1, nrow(origData2))
+                        ),
+                        tags$hr(),
+                        selectInput(
+                          "objective3",
+                          label = "By product category:Import",
+                          colnames(origData3)[-1],
+                          selectize = FALSE
+                        ),
+                        sliderInput(
+                          "dataRange3",
+                          label = "Data Range",
+                          min = 1,
+                          max = nrow(origData3),
+                          value = c(1, nrow(origData3))
+                        ),
+                        tags$hr(),
+                        selectInput(
+                          "objective4",
+                          label = "By Region:World",
+                          colnames(origData4)[-1],
+                          selectize = FALSE
+                        ),
+                        sliderInput(
+                          "dataRange4",
+                          label = "Data Range",
+                          min = 1,
+                          max = nrow(origData4),
+                          value = c(1, nrow(origData4))
+                        ),
+                        tags$hr(),
+                        radioButtons(
+                          "charttype",
+                          label = "Chart Type",
+                          choices = list(
+                            `Time series( Line )` = "l",
+                            `Time series( Bar )` = "h",
+                            Histogram = "hist",
+                            `Q-Q plot` = "QQ" ,
+                            `Arima` = "arima"
+                          ),
+                          selected = "l"
+                        ),
+                        tags$hr(),
+                        radioButtons(
+                          "datatype",
+                          label = "Data Type",
+                          choices = list(
+                            Level = 1,
+                            `1st difference` = 2  ,
+                            `2nd difference` = 3
+                          ),
+                          selected = 1
+                        )
+                      )
+                    ),
+                    column(
+                      10,
+                      fluidRow(column(6, wellPanel(
+                        tags$h4(textOutput("title01"), paste("Unit:trillion JPY")),
+                        plotOutput("plot01", height = "500px")
+                      )),
+                      column(6, wellPanel(
+                        tags$h4(textOutput("title02"), "Unit:100 million JPY"),
+                        plotOutput("plot02", height = "500px")
+                      ))),
+                      fluidRow(column(6, wellPanel(
+                        tags$h4(textOutput("title03"), "Unit:100 million JPY"),
+                        plotOutput("plot03", height = "500px")
+                      )),
+                      column(6, wellPanel(
+                        tags$h4(textOutput("title04"), paste("Unit:trillion JPY")),
+                        plotOutput("plot04", height = "500px")
+                      ))),
+                      tags$hr(),
+                      fluidRow(
+                        column(
+                          6,
+                          tags$h4(
+                            textOutput("title001"),
+                            paste("Unit of Monetary values:trillion JPY")
+                          ),
+                          DT::dataTableOutput("statisticTable01")
+                        ),
+                        column(
+                          6,
+                          tags$h4(
+                            textOutput("title002"),
+                            "Unit of Monetary values:100 million JPY"
+                          ),
+                          DT::dataTableOutput("statisticTable02")
+                        )
+                      ),
+                      tags$hr(),
+                      fluidRow(
+                        column(
+                          6,
+                          tags$h4(
+                            textOutput("title003"),
+                            "Unit of Monetary values:100 million JPY"
+                          ),
+                          DT::dataTableOutput("statisticTable03")
+                        ),
+                        column(
+                          6,
+                          tags$h4(textOutput("title004"), paste("Unit:trillion JPY")),
+                          DT::dataTableOutput("statisticTable04")
+                        )
+                      ),
+                      tags$hr(),
+                      fluidRow(
+                        column(
+                          6,
+                          tags$h4(
+                            textOutput("title0001"),
+                            paste("Unit of Monetary values:trillion JPY")
+                          ),
+                          DT::dataTableOutput("timeseriesTable01")
+                        ),
+                        column(
+                          6,
+                          tags$h4(
+                            textOutput("title0002"),
+                            "Unit of Monetary values:100 million JPY"
+                          ),
+                          DT::dataTableOutput("timeseriesTable02")
+                        )
+                      ),
+                      tags$hr(),
+                      fluidRow(
+                        column(
+                          6,
+                          tags$h4(
+                            textOutput("title0003"),
+                            "Unit of Monetary values:100 million JPY"
+                          ),
+                          DT::dataTableOutput("timeseriesTable03")
+                        ),
+                        column(
+                          6,
+                          tags$h4(textOutput("title0004"),
+                                  "Unit of Monetary values:trillion JPY"),
+                          DT::dataTableOutput("timeseriesTable04")
+                        )
+                      ),
+                      fluidRow(
+                        column(
+                          12,
+                          tags$hr(),
+                          paste("Unit of Monetary values:trillion JPY"),
+                          DT::dataTableOutput("allData"),
+                          htmlOutput("remarktext"),
+                          htmlOutput("history"),
+                          htmlOutput("gitcode")
+                        )
+                      )
+                    )
+                  )),
+           column(
+             2,
+             a(
+               "@AMC2_Japan",
+               class = "twitter-timeline"
+               ,
+               href = "https://twitter.com/AMC2_Japan"
+               ,
+               "data-widget-id" = "449799943780200448",
+               width = "100%",
+               height = "4500"
+             )
+           ))
+))
