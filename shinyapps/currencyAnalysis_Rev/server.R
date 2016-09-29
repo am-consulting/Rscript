9668c92db5cc0701198eb4c27581fb1c56258d3a
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-09-29 19:08:50 +0900

    変数名競合に起因する不具合回避対策
    変数名競合に起因する不具合回避対策

--- shinyapps/currencyAnalysis_Rev/server.R
+++ shinyapps/currencyAnalysis_Rev/server.R
@@ -15,6 +15,8 @@ shinyServer(function(input, output, session)
         ssl.verifypeer = F
       )
     eval(parse(text = script))
+    
+    origData_FXOanda <- origData
 
     latestDataDownloadTime_FXOanda <<- as.POSIXlt(Sys.time(), "GMT")    
 
@@ -23,14 +25,14 @@ shinyServer(function(input, output, session)
         "dataRange_FXOanda",
         label = "Period",
         min = 1,
-        max = nrow(origData),
-        value = c(1, nrow(origData)),
+        max = nrow(origData_FXOanda),
+        value = c(1, nrow(origData_FXOanda)),
         step = 1
       )
     })
 
     tab_FXOanda <<- 1
-    origData <<- origData
+    origData_FXOanda <<- origData_FXOanda
   })  
   
   observe({
@@ -40,55 +42,55 @@ shinyServer(function(input, output, session)
       if (is.null(input$dataRange_FXOanda[1])) { # dataRangeの読み込みを待つために必要
         return(NULL)
       } else{
-        dateFormat <- "%Y-%m-%d"
-        dateRange <- input$dataRange_FXOanda[2] - input$dataRange_FXOanda[1] + 1
-        if (dateRange < 10) { # adf.test: sample size must be greater than 8
+        dateFormat_FXOanda <- "%Y-%m-%d"
+        dateRange_FXOanda <- input$dataRange_FXOanda[2] - input$dataRange_FXOanda[1] + 1
+        if (dateRange_FXOanda < 10) { # adf.test: sample size must be greater than 8
           if (input$dataRange_FXOanda[2] < 10) {
-            lowerRange <- 1
-            upperRange <- 10
+            lowerRange_FXOanda <- 1
+            upperRange_FXOanda <- 10
           } else{
-            lowerRange <- input$dataRange_FXOanda[2] - 9
-            upperRange <- input$dataRange_FXOanda[2]
+            lowerRange_FXOanda <- input$dataRange_FXOanda[2] - 9
+            upperRange_FXOanda <- input$dataRange_FXOanda[2]
           }
         }
         else{
-          lowerRange <- input$dataRange_FXOanda[1]
-          upperRange <- input$dataRange_FXOanda[2]
+          lowerRange_FXOanda <- input$dataRange_FXOanda[1]
+          upperRange_FXOanda <- input$dataRange_FXOanda[2]
         }
         
-        dataSet <-
-          subset(origData,
-                 lowerRange <= index(origData) & index(origData) <= upperRange)
+        dataSet_FXOanda <-
+          subset(origData_FXOanda,
+                 lowerRange_FXOanda <= index(origData_FXOanda) & index(origData_FXOanda) <= upperRange_FXOanda)
 
-        dataSet01P <- data.frame(
-          Date = dataSet[-1,1],  
-          round(diff(as.matrix(dataSet[,-1]),lag = 1,differences = 1)/as.matrix(dataSet[-nrow(dataSet),-1])*100,2),
+        dataSet01P_FXOanda <- data.frame(
+          Date = dataSet_FXOanda[-1,1],  
+          round(diff(as.matrix(dataSet_FXOanda[,-1]),lag = 1,differences = 1)/as.matrix(dataSet_FXOanda[-nrow(dataSet_FXOanda),-1])*100,2),
           check.names = F
         )
         
-        colnames(dataSet01P)[-1]<-paste0(colnames(dataSet01P)[-1],'-DoD')
-        subtitle01 <- paste0(first(dataSet[,1]) ,'~', last(dataSet[,1]))
-        subtitle02 <- paste0(first(dataSet01P[,1]) ,'~', last(dataSet01P[,1]))
+        colnames(dataSet01P_FXOanda)[-1]<-paste0(colnames(dataSet01P_FXOanda)[-1],'-DoD')
+        subtitle01 <- paste0(first(dataSet_FXOanda[,1]) ,'~', last(dataSet_FXOanda[,1]))
+        subtitle02 <- paste0(first(dataSet01P_FXOanda[,1]) ,'~', last(dataSet01P_FXOanda[,1]))
         
         output$plot1_FXOanda <- renderPlot({
           par(mar = c(5, 5, 4, 3), mfrow=c(1, 3), oma=c(0, 0, 0, 0))
           for(iii in 2:4){
           plot(
-            dataSet[, 1],
-            dataSet[, iii],
+            dataSet_FXOanda[, 1],
+            dataSet_FXOanda[, iii],
             type = 'l',
             xlab = "",
-            ylab = colnames(dataSet)[iii],
+            ylab = colnames(dataSet_FXOanda)[iii],
             panel.first = grid(nx = NULL, ny = NULL, lty = 2, equilogs = T),
             xaxt = "n",
-            main = paste(colnames(dataSet)[iii], "\n", subtitle01),
+            main = paste(colnames(dataSet_FXOanda)[iii], "\n", subtitle01),
             cex.axis = 2,
             cex.lab = 2,
             cex.main = 2
           )
-          lo <- loess(dataSet[, iii]~as.numeric(dataSet[, 1]), degree = 2)
-          lines(dataSet[, 1] , predict(lo) , col='red', lwd=2, lty=2)            
-          axis.Date(side = 1, at = dataSet[, 1], format = dateFormat, padj = 1, cex.axis = 2)
+          lo <- loess(dataSet_FXOanda[, iii]~as.numeric(dataSet_FXOanda[, 1]), degree = 2)
+          lines(dataSet_FXOanda[, 1] , predict(lo) , col='red', lwd=2, lty=2)            
+          axis.Date(side = 1, at = dataSet_FXOanda[, 1], format = dateFormat_FXOanda, padj = 1, cex.axis = 2)
           } 
         })
           
@@ -96,19 +98,19 @@ shinyServer(function(input, output, session)
           par(mar = c(5, 5, 4, 3), mfrow=c(1, 3), oma=c(0, 0, 4, 0))
           for(iii in 2:4){
             plot(
-              dataSet01P[, 1],
-              dataSet01P[, iii],
+              dataSet01P_FXOanda[, 1],
+              dataSet01P_FXOanda[, iii],
               type = 'h',
               xlab = "",
-              ylab = colnames(dataSet01P)[iii],
+              ylab = colnames(dataSet01P_FXOanda)[iii],
               panel.first = grid(nx = NULL, ny = NULL, lty = 2, equilogs = T),
               xaxt = "n",
-              main = paste(colnames(dataSet01P)[iii], "\n", subtitle02),
+              main = paste(colnames(dataSet01P_FXOanda)[iii], "\n", subtitle02),
               cex.axis = 2,
               cex.lab = 2,
               cex.main = 2
             )
-            axis.Date(side = 1, at = dataSet01P[, 1], format = dateFormat, padj = 1, cex.axis = 2)
+            axis.Date(side = 1, at = dataSet01P_FXOanda[, 1], format = dateFormat_FXOanda, padj = 1, cex.axis = 2)
             abline(h=0, col='red', lwd=2)
           }
           mtext('Changes From Previous Day(%)',side = 3,outer = T,cex = 2)
@@ -117,28 +119,28 @@ shinyServer(function(input, output, session)
         correlationMethod<- c("pearson", "kendall", "spearman")
 
         output$title01_FXOanda <-   renderText({subtitle01})      
-        output$summary01_FXOanda <- renderPrint({summary(dataSet[,-1])})
-        output$psych01_FXOanda <- renderPrint({psych::describe(dataSet[,-1])})
-        output$pastecs01_FXOanda <- renderPrint({pastecs::stat.desc(dataSet[,-1])})
-        output$adf01_FXOanda <- renderPrint({apply(dataSet[,-1], 2, adf.test)})
+        output$summary01_FXOanda <- renderPrint({summary(dataSet_FXOanda[,-1])})
+        output$psych01_FXOanda <- renderPrint({psych::describe(dataSet_FXOanda[,-1])})
+        output$pastecs01_FXOanda <- renderPrint({pastecs::stat.desc(dataSet_FXOanda[,-1])})
+        output$adf01_FXOanda <- renderPrint({apply(dataSet_FXOanda[,-1], 2, adf.test)})
         
         output$title02_FXOanda <-   renderText({subtitle02})      
-        output$summary02_FXOanda <- renderPrint({summary(dataSet01P[,-1])})
-        output$psych02_FXOanda <- renderPrint({psych::describe(dataSet01P[,-1])})
-        output$pastecs02_FXOanda <- renderPrint({pastecs::stat.desc(dataSet01P[,-1])}) 
-        output$adf02_FXOanda <- renderPrint({apply(dataSet01P[,-1], 2, adf.test)})
+        output$summary02_FXOanda <- renderPrint({summary(dataSet01P_FXOanda[,-1])})
+        output$psych02_FXOanda <- renderPrint({psych::describe(dataSet01P_FXOanda[,-1])})
+        output$pastecs02_FXOanda <- renderPrint({pastecs::stat.desc(dataSet01P_FXOanda[,-1])}) 
+        output$adf02_FXOanda <- renderPrint({apply(dataSet01P_FXOanda[,-1], 2, adf.test)})
         
         selectedMethod01 <- 1
         output$title011corr_FXOanda <- output$title012corr_FXOanda <- renderText({correlationMethod[selectedMethod01]})
-        output$cor01_FXOanda <- renderPrint({cor(dataSet[,-1],    method = correlationMethod[selectedMethod01])})
-        output$cor02_FXOanda <- renderPrint({cor(dataSet01P[,-1], method = correlationMethod[selectedMethod01])})
+        output$cor01_FXOanda <- renderPrint({cor(dataSet_FXOanda[,-1],    method = correlationMethod[selectedMethod01])})
+        output$cor02_FXOanda <- renderPrint({cor(dataSet01P_FXOanda[,-1], method = correlationMethod[selectedMethod01])})
         
         selectedMethod02 <- 3
         output$title021corr_FXOanda <- output$title022corr_FXOanda <- renderText({correlationMethod[selectedMethod02]})
-        output$cor03_FXOanda <- renderPrint({cor(dataSet[,-1],    method = correlationMethod[selectedMethod02])})
-        output$cor04_FXOanda <- renderPrint({cor(dataSet01P[,-1], method = correlationMethod[selectedMethod02])})
+        output$cor03_FXOanda <- renderPrint({cor(dataSet_FXOanda[,-1],    method = correlationMethod[selectedMethod02])})
+        output$cor04_FXOanda <- renderPrint({cor(dataSet01P_FXOanda[,-1], method = correlationMethod[selectedMethod02])})
 
-        mergeData_FXOanda<- merge(dataSet,dataSet01P,by = 'Date',all=T)
+        mergeData_FXOanda<- merge(dataSet_FXOanda,dataSet01P_FXOanda,by = 'Date',all=T)
         
         output$table1_FXOanda <- DT::renderDataTable(
           mergeData_FXOanda,
