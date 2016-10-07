371650350893c48ce5294a96e8a2e55dedbecc19
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-10-07 13:19:54 +0900

    統合用海面水温新規アップロード
    統合用海面水温新規アップロード

---
+++ shinyapps/elninoindex_Rev/ui.R
@@ -0,0 +1,54 @@
+library(shiny)
+shinyUI(
+  fluidPage(
+    fluidRow(
+      column(10,
+             headerPanel("Sea Surface Temperature. Raw Data source: Japan Meteorological Agency"),
+             fluidRow(column(12, htmlOutput("figure01_SST"))),
+             fluidRow(
+               column(6,actionButton('searchAction_SST', label = 'Import Data(take a few seconds to import.)')),
+               column(6,align="right",textOutput('latestDataDownloadTime_SST'))
+             ),
+             conditionalPanel(condition = "output.latestDataDownloadTime_SST!=''",  tags$hr()), 
+             fluidRow(column(12, align='center', uiOutput("region_SST"))),
+             fluidRow(column(12,
+                             conditionalPanel(
+                               condition = "output.latestDataDownloadTime_SST!=''",	
+                               column(12,
+                                      h2(textOutput("datatitle_SST"))
+                               ),
+                               column(12,
+                                      column(6, plotOutput("plot1_SST")),
+                                      column(6, plotOutput("plot2_SST"))
+                               ),
+                               column(12,
+                                      column(6, plotOutput("plot4_SST")),
+                                      column(6, plotOutput("plot5_SST"))
+                                      
+                               ),
+                               column(12,tags$hr(),
+                                      plotOutput("plot3_SST")
+                               ),
+                               column(12,tags$hr(),
+                                      column(6, DT::dataTableOutput("table1_SST"))
+                               ),
+                               div(
+                                 "Caution: Rows containing missing values or non-finite values are removed.",
+                                 style = "color:black",
+                                 align = "center"
+                               )
+                             )   
+             )
+             ),
+             conditionalPanel(condition = "output.latestDataDownloadTime_SST!=''",  
+                              fluidRow(column(12, htmlOutput("remarktext_SST")))),  
+             fluidRow(column(12, htmlOutput("disclaimer_SST")))
+      ),
+      column(2,
+             a("@AMC2_Japan", class = "twitter-timeline",  href = "https://twitter.com/AMC2_Japan",
+               "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
+             )
+      )
+    ) 
+  )
+)
