8d8588bbb1b5373ad7a996f138af1e1e1f35083c
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-10-24 08:57:45 +0900

    1週間の発生地震統合版新規アップロード : ui.r
    1週間の発生地震統合版新規アップロード : ui.r

--- shinyapps/scienceData/ui.R
+++ shinyapps/scienceData/ui.R
@@ -13,6 +13,7 @@ library(xtable)
 library(rvest)
 library(psych)
 library(pastecs)
+library(Nippon)
 options(download.file.method = "libcurl")
 shinyUI(
   tabsetPanel(
@@ -47,10 +48,10 @@ shinyUI(
                  )
                  ) ,
                  fluidRow(
-                   column(12,                   
+                   column(12,
                           conditionalPanel(condition = "output.latestDataDownloadTimeEarthquake!=''",
                                            fluidRow(
-                                             column(6, 
+                                             column(6,
                                                     textOutput("titlegvisEarthquake"), htmlOutput("gvisEarthquake")
                                              ),
                                              column(6, DT::dataTableOutput("dtTableEarthquake"))
@@ -72,7 +73,7 @@ shinyUI(
                           fluidRow(column(12, htmlOutput("disclaimerEarthquake")))
                    )
                  )
-          ), 
+          ),
           column(2,
                  a("@AMC2_Japan", class = "twitter-timeline",  href = "https://twitter.com/AMC2_Japan",
                    "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
@@ -83,7 +84,7 @@ shinyUI(
     )
     # SeaIceIndex
     ,
-    tabPanel(      
+    tabPanel(
       "Sea Ice Index Data",
       fluidPage(
         fluidRow(
@@ -95,22 +96,22 @@ shinyUI(
                    column(6,actionButton('searchAction_SeaIceIndex', label = 'Import Data(take a few seconds to import.)')),
                    column(6,align="right",textOutput('latestDataDownloadTime_SeaIceIndex'))
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_SeaIceIndex != ''",  tags$hr()), 
+                 conditionalPanel(condition = "output.latestDataDownloadTime_SeaIceIndex != ''",  tags$hr()),
                  fluidRow(column(12, align='center', uiOutput("regionSeaIceIndex"))),
                  fluidRow(column(12,
                                  conditionalPanel(
-                                   condition = "output.latestDataDownloadTime_SeaIceIndex != ''",	
+                                   condition = "output.latestDataDownloadTime_SeaIceIndex != ''",
                                    column(12,
                                           h2(textOutput("datatitleSeaIceIndex")),
                                           column(4, wellPanel(plotOutput("plot1_SeaIceIndex"))),
                                           column(4, wellPanel(plotOutput("plot2_SeaIceIndex"))),
                                           column(4, wellPanel(plotOutput("plot3_SeaIceIndex")))
                                    )
-                                 )   
+                                 )
                  )
                  ),
-                 fluidRow(column(12, 
-                                 conditionalPanel(condition = "output.latestDataDownloadTime_SeaIceIndex != ''",  tags$hr(), 
+                 fluidRow(column(12,
+                                 conditionalPanel(condition = "output.latestDataDownloadTime_SeaIceIndex != ''",  tags$hr(),
                                                   wellPanel(div(style = "height:660px;", plotOutput("plot4_SeaIceIndex"))))
                  )
                  ),
@@ -118,10 +119,10 @@ shinyUI(
                                   fluidRow(column(12, wellPanel(DT::dataTableOutput("table1_SeaIceIndex")))), tags$hr(),
                                   tags$h3(textOutput("title01_SeaIceIndex")),
                                   verbatimTextOutput("summary01_SeaIceIndex"),
-                                  verbatimTextOutput("psych01_SeaIceIndex"), 
+                                  verbatimTextOutput("psych01_SeaIceIndex"),
                                   verbatimTextOutput("pastecs01_SeaIceIndex"),
                                   fluidRow(column(12, htmlOutput("remarktext_SeaIceIndex")))
-                 ),  
+                 ),
                  fluidRow(column(12, htmlOutput("disclaimer_SeaIceIndex")))
           ),
           column(2,
@@ -129,13 +130,13 @@ shinyUI(
                    "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
                  )
           )
-        ) 
+        )
       )
     )
     # SeaIceIndex
     # Sunspot
     ,
-    tabPanel(      
+    tabPanel(
       "Sunspot Number",
       fluidPage(
         fluidRow(
@@ -145,11 +146,11 @@ shinyUI(
                    column(6,actionButton('searchAction_Sunspot', label = 'Import Data(take a few seconds to import.)')),
                    column(6,align="right",textOutput('latestDataDownloadTime_Sunspot'))
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_Sunspot!=''",  tags$hr()), 
+                 conditionalPanel(condition = "output.latestDataDownloadTime_Sunspot!=''",  tags$hr()),
                  fluidRow(column(12, align='center', uiOutput("region_Sunspot"))),
                  fluidRow(column(12,
                                  conditionalPanel(
-                                   condition = "output.latestDataDownloadTime_Sunspot!=''",	
+                                   condition = "output.latestDataDownloadTime_Sunspot!=''",
                                    column(12, h3(textOutput("datatitle_Sunspot"))),
                                    column(12,
                                           column(6, wellPanel(plotOutput("plot1_Sunspot"))),
@@ -165,19 +166,19 @@ shinyUI(
                                    ),
                                    column(12,tags$hr(),
                                           verbatimTextOutput("summary01_Sunspot"),
-                                          verbatimTextOutput("psych01_Sunspot"), 
-                                          verbatimTextOutput("pastecs01_Sunspot")         
+                                          verbatimTextOutput("psych01_Sunspot"),
+                                          verbatimTextOutput("pastecs01_Sunspot")
                                    ),
                                    div(
                                      "Caution: Rows containing missing values or non-finite values are removed.",
                                      style = "color:black",
                                      align = "center"
                                    )
-                                 )   
+                                 )
                  )
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_Sunspot!=''",  
-                                  fluidRow(column(12, htmlOutput("remarktext_Sunspot")))),  
+                 conditionalPanel(condition = "output.latestDataDownloadTime_Sunspot!=''",
+                                  fluidRow(column(12, htmlOutput("remarktext_Sunspot")))),
                  fluidRow(column(12, htmlOutput("disclaimer_Sunspot")))
           ),
           column(2,
@@ -185,13 +186,13 @@ shinyUI(
                    "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
                  )
           )
-        ) 
+        )
       )
     )
-    # Sunspot  
+    # Sunspot
     # Carbon Dioxide Concentration
     ,
-    tabPanel(      
+    tabPanel(
       "CO2",
       fluidPage(
         fluidRow(
@@ -201,11 +202,11 @@ shinyUI(
                    column(6,actionButton('searchAction_CO2', label = 'Import Data(take a few seconds to import.)')),
                    column(6,align="right",textOutput('latestDataDownloadTime_CO2'))
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_CO2!=''",  tags$hr()), 
+                 conditionalPanel(condition = "output.latestDataDownloadTime_CO2!=''",  tags$hr()),
                  fluidRow(column(12, align='center', uiOutput("stationname_CO2"))),
                  fluidRow(column(12,
                                  conditionalPanel(
-                                   condition = "output.latestDataDownloadTime_CO2!=''",	
+                                   condition = "output.latestDataDownloadTime_CO2!=''",
                                    column(12,
                                           column(6, wellPanel(htmlOutput("gvis_CO2"))),
                                           column(6, DT::dataTableOutput("table1_CO2"))
@@ -213,7 +214,7 @@ shinyUI(
                                    column(12,
                                           column(6, wellPanel(plotOutput("plot1_CO2"))),
                                           column(6, wellPanel(plotOutput("plot2_CO2")))
-                                          
+
                                    ),
                                    column(12,tags$hr(),
                                           column(6, wellPanel(plotOutput("plot3_CO2"))),
@@ -224,11 +225,11 @@ shinyUI(
                                      style = "color:black",
                                      align = "center"
                                    )
-                                 )   
+                                 )
                  )
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_CO2!=''",  
-                                  fluidRow(column(12, htmlOutput("remarktext_CO2")))),  
+                 conditionalPanel(condition = "output.latestDataDownloadTime_CO2!=''",
+                                  fluidRow(column(12, htmlOutput("remarktext_CO2")))),
                  fluidRow(column(12, htmlOutput("disclaimer_CO2")))
           ),
           column(2,
@@ -236,13 +237,13 @@ shinyUI(
                    "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
                  )
           )
-        ) 
+        )
       )
     )
     # Carbon Dioxide Concentration
     # Monthly Mean Temperature Anomaly
     ,
-    tabPanel(      
+    tabPanel(
       "Monthly Mean Temperature",
       fluidPage(
         fluidRow(
@@ -252,11 +253,11 @@ shinyUI(
                    column(6,actionButton('searchAction_MMT', label = 'Import Data(take a few seconds to import.)')),
                    column(6,align="right",textOutput('latestDataDownloadTime_MMT'))
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_MMT!=''",  tags$hr()), 
+                 conditionalPanel(condition = "output.latestDataDownloadTime_MMT!=''",  tags$hr()),
                  fluidRow(column(12, align='center', uiOutput("region_MMT"))),
                  fluidRow(column(12,
                                  conditionalPanel(
-                                   condition = "output.latestDataDownloadTime_MMT!=''",	
+                                   condition = "output.latestDataDownloadTime_MMT!=''",
                                    column(12,
                                           h2(textOutput("datatitle_MMT"))
                                    ),
@@ -267,7 +268,7 @@ shinyUI(
                                    column(12,
                                           column(6, plotOutput("plot4_MMT")),
                                           column(6, plotOutput("plot5_MMT"))
-                                          
+
                                    ),
                                    column(12,tags$hr(),
                                           plotOutput("plot3_MMT")
@@ -281,11 +282,11 @@ shinyUI(
                                      style = "color:black",
                                      align = "center"
                                    )
-                                 )   
+                                 )
                  )
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_MMT!=''",  
-                                  fluidRow(column(12, htmlOutput("remarktext_MMT")))),  
+                 conditionalPanel(condition = "output.latestDataDownloadTime_MMT!=''",
+                                  fluidRow(column(12, htmlOutput("remarktext_MMT")))),
                  fluidRow(column(12, htmlOutput("disclaimer_MMT")))
           ),
           column(2,
@@ -293,14 +294,14 @@ shinyUI(
                    "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
                  )
           )
-        ) 
-      )      
+        )
+      )
     )
     # Monthly Mean Temperature Anomaly
     # Sea Surface Temperature
     ,
-    tabPanel(      
-      "Sea Surface Temperature",  
+    tabPanel(
+      "Sea Surface Temperature",
       fluidPage(
         fluidRow(
           column(10,
@@ -310,11 +311,11 @@ shinyUI(
                    column(6,actionButton('searchAction_SST', label = 'Import Data(take a few seconds to import.)')),
                    column(6,align="right",textOutput('latestDataDownloadTime_SST'))
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_SST!=''",  tags$hr()), 
+                 conditionalPanel(condition = "output.latestDataDownloadTime_SST!=''",  tags$hr()),
                  fluidRow(column(12, align='center', uiOutput("region_SST"))),
                  fluidRow(column(12,
                                  conditionalPanel(
-                                   condition = "output.latestDataDownloadTime_SST!=''",	
+                                   condition = "output.latestDataDownloadTime_SST!=''",
                                    column(12,
                                           h2(textOutput("datatitle_SST"))
                                    ),
@@ -330,9 +331,9 @@ shinyUI(
                                           plotOutput("plot3_SST")
                                    ),
                                    column(12,tags$hr(),
-                                          column(6, DT::dataTableOutput("table1_SST"), 
+                                          column(6, DT::dataTableOutput("table1_SST"),
                                                  verbatimTextOutput("adf_SST"), verbatimTextOutput("psych_SST")),
-                                          column(6, verbatimTextOutput("summary_SST"), 
+                                          column(6, verbatimTextOutput("summary_SST"),
                                                  verbatimTextOutput("summary_fit_SST"), verbatimTextOutput("confint_SST"))
                                    ),
                                    div(
@@ -340,11 +341,11 @@ shinyUI(
                                      style = "color:black",
                                      align = "center"
                                    )
-                                 )   
+                                 )
                  )
                  ),
-                 conditionalPanel(condition = "output.latestDataDownloadTime_SST!=''",  
-                                  fluidRow(column(12, htmlOutput("remarktext_SST")))),  
+                 conditionalPanel(condition = "output.latestDataDownloadTime_SST!=''",
+                                  fluidRow(column(12, htmlOutput("remarktext_SST")))),
                  fluidRow(column(12, htmlOutput("disclaimer_SST")))
           ),
           column(2,
@@ -352,9 +353,45 @@ shinyUI(
                    "data-widget-id" = "449799943780200448", width = "100%", height = "1200"
                  )
           )
-        ) 
+        )
       )
     )
     # Sea Surface Temperature
+    # Earthquakes for the last week in Japan
+    ,
+    tabPanel(
+      "Earthquakes for the last week in Japan",
+      fluidPage(
+        fluidRow(
+          column(10,
+                 headerPanel("Earthquakes for the last week in Japan. Raw Data source: Japan Meteorological Agency"),
+                 fluidRow(
+                   column(6,actionButton('searchAction_1weekEQ', label = 'Import Data(take a few seconds to import.)')),
+                   column(6,align="right",textOutput('latestDataDownloadTime_1weekEQ'))
+                 ),
+                 conditionalPanel(condition = "output.latestDataDownloadTime_1weekEQ!=''",  tags$hr()),
+                 fluidRow(column(12,
+                                 conditionalPanel(
+                                   condition = "output.latestDataDownloadTime_1weekEQ!=''",
+                                   column(12,
+                                          column(6, DT::dataTableOutput("table1_1week01EQ")),
+                                          column(6, DT::dataTableOutput("table2_1week02EQ"))
+                                   )
+                                 )
+                 )
+                 ),
+                 conditionalPanel(condition = "output.latestDataDownloadTime_1weekEQ!=''",
+                                  fluidRow(column(12, htmlOutput("remarktext_1weekEQ")))),
+                 fluidRow(column(12, htmlOutput("disclaimer_1weekEQ")))
+          ),
+          column(2,
+                 a("@AMC2_Japan", class = "twitter-timeline",  href = "https://twitter.com/AMC2_Japan",
+                   "data-widget-id" = "449799943780200448", width = "100%", height = "1000"
+                 )
+          )
+        )
+      )
+    )
+    # Earthquakes for the last week in Japan
   )
 )
