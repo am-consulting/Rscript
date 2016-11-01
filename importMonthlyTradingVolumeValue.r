2d818666bb8fd725727430216943be7437c59a6f
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-11-01 14:15:20 +0900

    売買高改良
    売買高改良

--- importMonthlyTradingVolumeValue.r
+++ importMonthlyTradingVolumeValue.r
@@ -6,26 +6,22 @@ options(download.file.method='libcurl')
 perl <- gdata:::findPerl('perl')
 dataURL <-
   c('http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq00000023wp-att/historical-genbutsu.xls')
-for (iii in 1:length(dataURL)) { 
-  Sys.sleep(1) #avoid to overload    
-  switch (iii,
-          objsheet <- 1)
-  switch (iii,
-          objcolumn <- c(2:8))
+  Sys.sleep(1) #avoid to overload
   buf0 <-
     read.xls(
-      dataURL[iii],
+      dataURL[1],
       perl = perl,
       check.names = F,
       header = F,
       stringsAsFactors = F,
-      sheet = objsheet,
+      sheet = 1,
       fileEncoding = 'utf-8'
     )
-  buf <- buf0[-c(1:9),c(1,4)]
+  buf1 <- buf0[,1:(c(grep('二部',buf0[6,]))-1)]
+  buf2 <- buf1[,c(1,grep('平均',buf1))]
+  buf  <- buf2[-c(1:9),]
   buf[,1] <- as.Date(paste0(buf[,1],'/1'))
-  colnames(buf)<-c('Date','一部･1日平均売買高(億株)')
-  buf[,-1]<-apply(buf[,-1,drop=F],2,function(x)as.numeric(gsub(',','',x))*10^-5)
-  assign(paste0('MonthlyTradingVolumeValue'), buf, envir = .GlobalEnv)
-  gc();gc()
-}
+  colnames(buf)<-c('Date','一部･1日平均売買高(億株)','一部･1日平均売買代金高(兆円)')
+  buf[,2]<-apply(buf[,2,drop=F],2,function(x)as.numeric(gsub(',','',x))*10^-5)
+  buf[,3]<-apply(buf[,3,drop=F],2,function(x)as.numeric(gsub(',','',x))*10^-6)
+  assign(paste0('monthlyTradingVolumeValue'), buf, envir = .GlobalEnv)
