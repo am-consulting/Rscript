c97c7478c6036161d5822d71ecbd53714d03fe73
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-11-01 15:50:30 +0900

    市場追記
    市場追記

--- importAverageYield.r
+++ importAverageYield.r
@@ -7,7 +7,6 @@ perl <- gdata:::findPerl('perl')
 dataURL <-
   c('http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001w9e-att/historical-y.xls')
   Sys.sleep(1) #avoid to overload
-  objcolumn <- c(1:7)
   buf0 <-
     read.xls(
       dataURL[1],
@@ -18,8 +17,8 @@ dataURL <-
       sheet = 1,
       fileEncoding = 'utf-8'
     )
-  buf1 <- buf0[,objcolumn]
-  colnames(buf1) <- paste0(buf1[1,1],'-',gsub('\n','',unlist(buf1[5, ])))
+  buf1 <- buf0[,1:(grep('二部',buf0[4,])-1)]
+  colnames(buf1) <- paste0(buf1[1,1],'-第一部-',gsub('\n','',unlist(buf1[5, ])))
   buf2 <- buf1[-c(1:5),]
   colnames(buf2)[1]<-'Date'
   buf2[, 1] <- as.Date(paste0(buf2[,1],'/1'))
