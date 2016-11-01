5731203ffe83b4af2c9c35b45724e0a0d93f6a47
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-11-01 14:11:18 +0900

    株式時価総額改良
    株式時価総額改良

--- importMarketCapitalization.r
+++ importMarketCapitalization.r
@@ -6,27 +6,22 @@ options(download.file.method='libcurl')
 perl <- gdata:::findPerl('perl')
 dataURL <-
   c('http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001w3y-att/historical-jika.xls')
-for (iii in 1:length(dataURL)) { 
-  Sys.sleep(1) #avoid to overload    
-  switch (iii,
-          objsheet <- 1)
-  switch (iii,
-          objcolumn <- c(2:8))
-  buf <-
+  Sys.sleep(1) #avoid to overload
+  objcolumn <- c(2:8)
+  buf0 <-
     read.xls(
-      dataURL[iii],
+      dataURL[1],
       perl = perl,
       check.names = F,
       header = F,
       stringsAsFactors = F,
-      sheet = objsheet,
+      sheet = 1,
       fileEncoding = 'utf-8'
     )
-  buf <- buf[, c(1, objcolumn)]
-  colnames(buf) <-  gsub('\n','',unlist(buf[3, ]))
-  buf <- buf[-c(1:3), ]
-  buf[,-1]<-apply(buf[,-1],2,function(x)as.numeric(gsub(',','',x))*10^-6)
-  buf[,1]<-as.Date(buf[,1])
-  assign(paste0('MarketCapitalization'), buf, envir = .GlobalEnv)
-  gc();gc()
-}
+  buf1 <- buf0[, c(1, objcolumn)]
+  colnames(buf1) <-  paste0(buf1[1,1],'-',gsub('\n','',unlist(buf1[3, ])))
+  buf2 <- buf1[-c(1:3), ]
+  buf2[,-1] <- apply(buf2[,-1],2,function(x)as.numeric(gsub(',','',x))*10^-6) # 兆円
+  buf2[,1]  <- as.Date(buf2[,1])
+  colnames(buf2)[1] <- 'Date'
+  assign(paste0('marketCapitalization'), buf2, envir = .GlobalEnv)
