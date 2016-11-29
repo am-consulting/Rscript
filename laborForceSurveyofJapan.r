4facc8ea7eb35b8ceaf605d80ff81093b1e66801
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-11-29 14:25:25 +0900

    ソースデータ取得方法変更
    ソースデータ取得方法変更

--- laborForceSurveyofJapan.r
+++ laborForceSurveyofJapan.r
@@ -1,24 +1,31 @@
 # License:GPL(version 2 or later)
 # Data Source:Statistics Bureau, Ministry of Internal Affairs and Communications
-library(gdata)
+# library(gdata);library(excel.link);
+library(XLConnect)
 options(download.file.method = "libcurl")
-perl <- gdata:::findPerl("perl")
-dataURL <-
-  c("http://www.stat.go.jp/data/roudou/longtime/zuhyou/lt01-a10.xls",1)
-for (iii in seq(1, length(dataURL), by = 2)) {
-  buf <-
-    read.xls(
-      dataURL[iii],
-      perl = perl,
-      check.names = F,
-      header = F,
-      stringsAsFactors = F,
-      sheet = dataURL[iii + 1],
-      fileEncoding = "utf-8"
-    )
-  gc()
-  gc()
-}
+# perl <- gdata:::findPerl("perl")
+username <- Sys.info()['user']
+pathOutput <- paste0("C:/Users/", username, "/Desktop/R_Data_Write/")
+setwd(pathOutput)
+fileName <- 'lt01-a10.xls'
+dataURL <- c("http://www.stat.go.jp/data/roudou/longtime/zuhyou/",'季節調整値')#1) # sheet1:季節調整値
+download.file(paste0(dataURL[1],fileName), fileName , mode = 'wb')
+buf <-
+  readWorksheetFromFile(paste0(pathOutput, fileName), sheet = iconv(dataURL[2],'shift_jis','utf8'), check.names = F, header = F)
+# for (iii in seq(1, length(dataURL), by = 2)) {
+#   buf <-
+#     read.xls(
+#       dataURL[iii],
+#       perl = perl,
+#       check.names = F,
+#       header = F,
+#       stringsAsFactors = F,
+#       sheet = iconv(dataURL[iii + 1],'shift_jis','utf8'),
+#       fileEncoding = "utf-8"
+#     )
+#   gc()
+#   gc()
+# }
 for (rrr in 1:nrow(buf)) {
   tmp <- as.numeric(substr(buf[rrr, 1], 1, 4))
   if (!is.na(tmp)) {
@@ -26,26 +33,23 @@ for (rrr in 1:nrow(buf)) {
   }
 }
 year <- tmp
-# rrr <- rrr - 4 #caution English
-rrr <- rrr - 5 #caution Japanese
-bufdataSet <- buf[rrr:nrow(buf), 5:ncol(buf)] #caution
+rrr <- rrr - 6
+bufdataSet <- buf[rrr:nrow(buf), 5:ncol(buf)]
 for (ccc in 1:ncol(bufdataSet)) {
-  if (bufdataSet[1, ccc] != "") {
+  if (!is.na(bufdataSet[1, ccc]) & bufdataSet[1, ccc] != "") {
     tmpcolname <- bufdataSet[1, ccc]
   }
   bufdataSet[3, ccc] <-
     paste(tmpcolname, "-",  bufdataSet[3, ccc], sep = "")
 }
 colnames(bufdataSet) <- bufdataSet[3, ]
-# bufdataSet <- bufdataSet[-(1:3), ] # English
-bufdataSet <- bufdataSet[-(1:4), ] # Japanese
+bufdataSet <- bufdataSet[-(1:5), ]
 startDate <- as.Date(paste(year, "-1-1", sep = ""))
 origData <-
-  data.frame(Date = as.Date(seq(
-    startDate, length.out = nrow(bufdataSet), by = "months"
-  )), bufdataSet, check.names = F)
+  data.frame(Date = as.Date(seq(startDate, length.out = nrow(bufdataSet), by = "months")), bufdataSet, check.names = F)
 origData[, -1] <-
   apply(origData[, -1], 2, function(x) {
     as.numeric(gsub("\\(|\\)|<|>", "", x)) #caution
   })
-origData <<- origData
+colnames(origData)[-1] <- paste0(colnames(origData)[-1],'-',dataURL[2])
+assign('laborforcesurvey', na.omit(origData), envir = .GlobalEnv)
