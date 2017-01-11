de9ed6d574550a6be7897e8130beffd134f89c72
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-11 21:55:08 +0900

    全面的変更
    全面的変更

--- importGDPofJapan.r
+++ importGDPofJapan.r
@@ -38,49 +38,19 @@ GDPList0 <- rbind(
   GDPList[grep('実質季節調整系列\\(前期比\\)',GDPList[,2])[1],],
   GDPList[grep('実質暦年\\(前年比\\)',GDPList[,2])[1],])
 url <- GDPList0[,1]
-titleJ <- GDPList0[,2]
-appendJ <- substring(GDPList0[,2],1,sapply(GDPList0[,2],function(x)regexpr('csv',x,ignore.case = T))-2)
-colnameDataJ <- c(
-  "国内総生産(支出側)",
-  "民間最終消費支出",
-  "家計最終消費支出",
-  "家計最終消費支出-除く持ち家の帰属家賃",
-  "民間住宅",
-  "民間企業設備",
-  "民間在庫品増加",
-  "政府最終消費支出",
-  "公的固定資本形成",
-  "公的在庫品増加",
-  "財貨･サービス-純輸出",
-  "財貨･サービス-輸出",
-  "財貨･サービス-輸入"
-)
-colnameDataE <- c(
-  "GDP(Expenditure Approach)",
-  "PrivateConsumption",
-  "Consumption ofHouseholds",
-  "ExcludingImputed Rent",
-  "PrivateResidentialInvestment",
-  "Private Non-Resi.Investment",
-  "Changein PrivateInventories",
-  "GovernmentConsumption",
-  "PublicInvestment",
-  "Changein PublicInventories",
-  "Goods & Services-Net Exports",
-  "Goods & Services-Exports",
-  "Goods & Services-Imports"
-)
+sheetTitlesJ <- sheetTitlesE <- vector()
 for (uuu in 1:length(url)) {
-  tmp <-
-    read.csv(
-      url[uuu],
-      header = F,
-      skip = 0,
-      stringsAsFactor = F,
-      na.strings = c("", "***"),
-      check.names = F,
-      fileEncoding = "cp932"
-    )
+  assign(paste0('tmpGDP',uuu),
+         read.csv(
+           url[uuu],
+           header = F,
+           skip = 0,
+           stringsAsFactor = F,
+           na.strings = c("", "***"),
+           check.names = F,
+           fileEncoding = "cp932")
+         )
+  tmp <- get(paste0('tmpGDP',uuu))
   firstDateRow <-
     which(!is.na(as.numeric(substring(tmp[,1],1,4))))[1]
   buf <-
@@ -96,15 +66,16 @@ for (uuu in 1:length(url)) {
     DateColumn <-
       seq(as.Date(paste0(firstYear,'-',firstMonth,'-1')), by = "12 month", length.out = nrow(tmp)-firstDateRow +1)
   }
-  sheetTitle <- zen2han(tmp[1,1])
+  sheetTitlesJ[uuu] <- zen2han(tmp[1,1])
+  sheetTitlesE[uuu] <- tmp[2,1]
   valueColumn <-
     tmp[-c(1:(firstDateRow - 1)), -1]
   dataSetWithoutColnames <-
     data.frame(DateColumn,valueColumn,stringsAsFactors = F)
   endColumn <-
     grep('参考',tmp[1,])-2
-  dataSetWithoutColnames <- dataSetWithoutColnames[,1:(endColumn-1)]
-  tmp0 <- tmp[,1:(endColumn-1)]
+  dataSetWithoutColnames <- dataSetWithoutColnames[,1:endColumn]
+  tmp0 <- tmp[,1:endColumn]
   buf <- NA
   for(ccc in 1:ncol(tmp0)){
     if(!is.na(tmp0[3,ccc])){buf <- tmp0[3,ccc]}
@@ -116,13 +87,16 @@ for (uuu in 1:length(url)) {
     if(length(grep('民間最終消費支出', tmp0[3,ccc]))!=0){tmp0[4,ccc] <- buf}
   }
   dataSetColnames <-
-    paste0(sheetTitle,':',sapply(gsub(':na','',paste0(tmp0[3,],':',tmp0[4,],':',tmp0[5,]),ignore.case = T),zen2han))
+    paste0(sheetTitlesJ[uuu],':',sapply(gsub(':na',
+                                            '',
+                                            paste0(tmp0[3,],':',tmp0[4,],':',tmp0[5,]),ignore.case = T),
+                                       zen2han))
   colnames(dataSetWithoutColnames) <- dataSetColnames
   colnames(dataSetWithoutColnames)[1] <- 'Date'
   dataSetWithoutColnames[,-1] <-
     apply(dataSetWithoutColnames[,-1],2,function(x)as.numeric(gsub(',','',x)))
   dataSet <-
-    dataSetWithoutColnames[-which((ncol(dataSetWithoutColnames)-1) <=
+    dataSetWithoutColnames[which((ncol(dataSetWithoutColnames)-1) >
                                   apply(dataSetWithoutColnames,1,function(x)sum(is.na(x)))),]
   assign(paste("gdp", uuu, sep = ""), dataSet, envir = .GlobalEnv)
 }
