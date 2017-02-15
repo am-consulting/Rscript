6e2183441ee0c126f80491fcc4ffe1eea857a3cd
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-02-15 13:19:03 +0900

    ソースエクセルデータ変更に伴う変更
    ソースエクセルデータ変更に伴う変更

--- importConstructionStatistics.r
+++ importConstructionStatistics.r
@@ -18,41 +18,51 @@ for(fff in 1:length(xlsList)){
   if(length(grep('住宅・建築物',titleList[fff]))!=0){
     download.file(paste0(url0, xlsList[fff]), xlsList[fff], mode = "wb")
     assign(paste0('origData',formatC(fff,width=2,flag="0")),
-           readWorksheetFromFile(paste0(pathOutput, xlsList[fff]), sheet = 1, check.names = F, header = F),envir = .GlobalEnv)
+           readWorksheetFromFile(paste0(pathOutput, xlsList[fff]), sheet = 1, check.names = F, header = F),
+           envir = .GlobalEnv)
   }
 }
 # 原数値パート
 fff <- 1
 buf01 <- get(paste0('origData',formatC(fff,width=2,flag="0")))
-buf01 <- buf01[,1:3]
 title01 <- buf01[1,2]
-buf01[buf01=='　'] <- NA
-colnames(buf01) <- buf01[7,]
-buf01 <- buf01[-c(1:7),]
-buf01 <- na.omit(buf01)
-buf01[,1] <- unlist(lapply(buf01[,1],zen2han))
-bufrrr <- tail(grep('H',buf01[,1]),1)
-yyyy <- as.numeric(gsub('H','',substring(buf01[bufrrr,1],1,3))) + 1988
-if(nrow(buf01) != bufrrr){mm <- as.numeric(tail(buf01[,1],1))}else{mm <- as.numeric(gsub('月','',substring(buf01[bufrrr,1],5)))}
-rrrS <- which(buf01[,1]=='S41年 1月')
-buf01 <- buf01[rrrS:nrow(buf01),]
-buf01[,1] <- rev(seq(as.Date(paste0(yyyy,'-',mm,'-1')),by = '-1 month',length.out = nrow(buf01)))
-colnames(buf01) <- c('Date','新設住宅着工戸数(戸)','新設住宅着工戸数前年比(%)')
-buf01[,-1] <- apply(buf01[,-1],2,function(x)as.numeric(gsub(',','',x)))
+buf01 <- buf01[,1:3]
+buf01 <- buf01[!is.na(buf01[,1]),]
+buf01 <- buf01[!is.na(buf01[,2]),]
+buf01[,1] <- gsub('\\s','',sapply(buf01[,1],zen2han))
+buf01 <- buf01[grep('S[0-9]+年',buf01[,1],ignore.case = T)[1]:nrow(buf01),]
+yyyy <-
+  as.numeric(gsub('S([0-9]+)年.+','\\1',buf01[1,1],ignore.case = T)) + 1925
+mm <-
+  as.numeric(gsub('S.+年([0-9]+)月','\\1',buf01[1,1],ignore.case = T))
+buf01[,1] <-
+  seq(as.Date(paste0(yyyy,'-',mm,'-1')),by = '+1 month',length.out = nrow(buf01))
+colnames(buf01) <-
+  c('Date','新設住宅着工戸数(戸)','新設住宅着工戸数前年比(%)')
+buf01[,-1] <-
+  apply(buf01[,-1],2,function(x)as.numeric(gsub(',','',x)))
+row.names(buf01) <- NULL
 NSAData <- buf01
 # 原数値パート
 # 季節調整パート
 fff <- 2
 buf02 <- get(paste0('origData',formatC(fff,width=2,flag="0")))
-buf02 <- buf02[,1:3]
 title02 <- buf02[1,2]
-buf02[buf02=='　'] <- NA
-colnames(buf02) <- buf02[3,]
-buf02 <- buf02[-c(1:5),]
-buf02 <- na.omit(buf02)
-buf02 <- tail(buf02,nrow(buf01))
-buf02[,1] <- buf01[,1]
-colnames(buf02) <- c('Date','新設住宅着工戸数(戸,季節調整値)','新設住宅着工戸数季節調整値前月比(%)')
-buf02[,-1] <- apply(buf02[,-1],2,function(x)as.numeric(gsub(',','',x)))
+buf02 <- buf02[,1:3]
+buf02 <- buf02[!is.na(buf02[,1]),]
+buf02 <- buf02[!is.na(buf02[,2]),]
+buf02[,1] <- gsub('\\s','',sapply(buf02[,1],zen2han))
+buf02 <- buf02[grep('[0-9]+年',buf02[,1],ignore.case = T)[1]:nrow(buf02),]
+yyyy <-
+  as.numeric(gsub('([0-9]+)年.+','\\1',buf02[1,1],ignore.case = T)) + 1925
+mm <-
+  as.numeric(gsub('.+年([0-9]+)月','\\1',buf02[1,1],ignore.case = T))
+buf02[,1] <-
+  seq(as.Date(paste0(yyyy,'-',mm,'-1')),by = '+1 month',length.out = nrow(buf02))
+colnames(buf02) <-
+  c('Date','新設住宅着工戸数(戸,季節調整値)','新設住宅着工戸数季節調整値前月比(%)')
+buf02[,-1] <-
+  apply(buf02[,-1],2,function(x)as.numeric(gsub(',','',x)))
+row.names(buf02) <- NULL
 SAData <- buf02
 # 季節調整パート
