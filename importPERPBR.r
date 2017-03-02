8d748c0df8319de99ccdfd501a75bd5b8c2e5b9a
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-03-02 19:26:43 +0900

    日付列修正
    日付列修正

--- importPERPBR.r
+++ importPERPBR.r
@@ -1,6 +1,6 @@
 # License:GPL(version 2 or later)
 # Data Source:Japan Exchange Group, Inc.
-library(gdata)
+library(gdata);library(lubridate)
 options(download.file.method='libcurl')
 
 perl <- gdata:::findPerl('perl')
@@ -22,11 +22,11 @@ dataURL <-
     )
   buf1 <- buf0[,objcolumn]
   colnames(buf1) <- gsub('\n','',unlist(buf1[2, ]))
-  buf2 <- buf1[,1:(grep('二部',buf1[1,])-1)]; title <- paste0(title,'-市場第一部')
+  buf2 <- buf1[,1:(grep('二部',buf1[1,])-1)]; title <- paste0(title,':市場第一部')
   buf3 <- buf2[-c(1:2),]
-  buf3[,1] <- as.Date(paste0(buf3[,1],'-',buf3[,3],'-',buf3[,2]))
+  buf3[,1] <- as.Date(paste0(buf3[,1],'-',buf3[,3],'-1')) %m+% months(1) - 1
   buf4 <- buf3[,c(1,5,6)]
   buf4[,-1] <- apply(buf4[,-1,drop=F],2,function(x)as.numeric(gsub(',','',x)))
-  colnames(buf4)[-1] <- paste0(title,'-',colnames(buf4)[-1])
+  colnames(buf4)[-1] <- paste0(title,':',colnames(buf4)[-1])
   colnames(buf4)[1] <- 'Date'
   assign(paste0('PERPBR'), buf4, envir = .GlobalEnv)
