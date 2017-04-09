e2779db35c95e11236a135f18251e92efcbd5eeb
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-09 11:54:08 +0900

    対象を全系列に変更
    対象を全系列に変更

--- importMonthlyTradingVolumeValue.r
+++ importMonthlyTradingVolumeValue.r
@@ -1,13 +1,13 @@
 # License:GPL(version 2 or later)
 # Data Source:Japan Exchange Group, Inc.
-library(gdata)
+library(gdata);library(Nippon)
 options(download.file.method='libcurl')
 
 perl <- gdata:::findPerl('perl')
 dataURL <-
   c('http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq00000023wp-att/historical-genbutsu.xls')
   Sys.sleep(1) #avoid to overload
-  buf0 <-
+  buf <-
     read.xls(
       dataURL[1],
       perl = perl,
@@ -15,13 +15,39 @@ dataURL <-
       header = F,
       stringsAsFactors = F,
       sheet = 1,
-      fileEncoding = 'utf-8'
+      fileEncoding = 'utf-8',
+      na.strings = c('')
     )
-  buf1 <- buf0[,1:(c(grep('二部',buf0[6,]))-1)]
-  buf2 <- buf1[,c(1,grep('平均',buf1))]
-  buf  <- buf2[-c(1:9),]
-  buf[,1] <- as.Date(paste0(buf[,1],'/1'))
-  colnames(buf)<-c('Date','一部･1日平均売買高(億株)','一部･1日平均売買代金高(兆円)')
-  buf[,2] <- sapply(buf[,2],function(x)as.numeric(gsub(',','',x))*10^-5)
-  buf[,3] <- sapply(buf[,3],function(x)as.numeric(gsub(',','',x))*10^-6)
-  assign(paste0('monthlyTradingVolumeValue'), buf, envir = .GlobalEnv)
+  buf0 <- buf
+  for(rrr in 5:9){
+    buf0[rrr,] <-
+      gsub('([^\\s]+)\\s.+','\\1',gsub('([^\n]+)\n.+','\\1',buf0[rrr,]))
+  }
+  tmp <- NA
+  for(ccc in seq(ncol(buf0))){
+    if(!is.na(buf0[5,ccc])){tmp <- buf0[5,ccc]}
+    buf0[5,ccc] <- tmp
+  }
+  tmp <- NA
+  for(ccc in seq(ncol(buf0))){
+    if(!is.na(buf0[6,ccc])){tmp <- buf0[6,ccc]}
+    if(length(grep('内国株式',buf0[5,ccc]))!=0){buf0[6,ccc] <- tmp}
+  }
+  tmp <- NA
+  for(ccc in seq(ncol(buf0))){
+    if(!is.na(buf0[7,ccc])){tmp <- buf0[7,ccc]}
+    buf0[7,ccc] <- tmp
+  }
+  colnames(buf0) <-
+    sapply(gsub(':na','',paste0(buf0[5,],':',buf0[6,],':',buf0[7,],':',buf0[8,],':',buf0[9,]),ignore.case = T),zen2han)
+  colnames(buf0) <-
+    gsub('立会','立会日数',colnames(buf0))
+  buf1 <- buf0[-c(1:9),]
+  Date <-
+    as.Date(gsub('([0-9]+)/([0-9]+)','\\1-\\2-1',buf1[,1]))
+  buf2 <-
+    data.frame(Date,apply(buf1[,-1],2,function(x)as.numeric(gsub(',','',x))),
+               check.names = F,row.names = NULL,stringsAsFactors = F)
+  buf3 <-
+    buf2[,apply(buf2,2,function(x)sum(is.na(x)))!=nrow(buf2)]
+  assign(paste0('monthlyTradingVolumeValue'), buf3, envir = .GlobalEnv)
