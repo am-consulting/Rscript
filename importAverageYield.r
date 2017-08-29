c817459a5cf498fa21e9d2c17d217fa48014e8a6
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-08-29 20:34:33 +0900

    列名から英字を削除
    列名から英字を削除

--- importAverageYield.r
+++ importAverageYield.r
@@ -1,6 +1,6 @@
 # License:GPL(version 2 or later)
 # Data Source:Japan Exchange Group, Inc.
-library(gdata)
+library(gdata);library(Nippon)
 options(download.file.method='libcurl')
 
 perl <- gdata:::findPerl('perl')
@@ -15,12 +15,20 @@ dataURL <-
       header = F,
       stringsAsFactors = F,
       sheet = 1,
-      fileEncoding = 'utf-8'
-    )
-  buf1 <- buf0[,1:(grep('二部',buf0[4,])-1)]
-  colnames(buf1) <- paste0(buf1[1,1],'-第一部-',gsub('\n','',unlist(buf1[5, ])))
-  buf2 <- buf1[-c(1:5),]
-  colnames(buf2)[1]<-'Date'
-  buf2[, 1] <- as.Date(paste0(buf2[,1],'/1'))
-  buf2[,-1] <- apply(buf2[,-1,drop=F],2,as.numeric)
-  assign(paste0('averageYield'), buf2, envir = .GlobalEnv)
+      fileEncoding = 'utf-8')
+  buf1 <- buf0
+  buf1[4,] <- gsub('[a-zA-Z0-9\\.]+|\n|\\s','',buf1[4,])
+  buf1[5,] <- gsub('[a-zA-Z0-9\\.]+|\n|\\s','',buf1[5,])
+  buf2 <- buf1[,!is.na(buf1[1,])]
+  tmp <- NA
+  for(ccc in seq(ncol(buf2))){
+    if(buf2[4,ccc]!=''){tmp <- buf2[4,ccc]}
+    buf2[4,ccc] <- tmp
+  }
+  colnames(buf2) <- sapply(paste0(buf2[4,],':',buf2[5,],buf2[3,ncol(buf2)]),zen2han)
+  buf3 <- buf2[!is.na(as.numeric(buf2[,2])),]
+  colnames(buf3)[1] <- 'Date'
+  buf3[, 1] <- as.Date(paste0(buf3[,1],'/1'))
+  buf3[,-1] <- apply(buf3[,-1,drop=F],2,as.numeric)
+  row.names(buf3) <- NULL
+  assign(paste0('averageYield'), buf3, envir = .GlobalEnv)
