a349fa22eafc420a6d9d21923b575ecfee7b7938
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-08-29 21:01:19 +0900

    列名から英字を削除
    列名から英字を削除

--- importMarketCapitalization.r
+++ importMarketCapitalization.r
@@ -1,13 +1,12 @@
 # License:GPL(version 2 or later)
 # Data Source:Japan Exchange Group, Inc.
-library(gdata)
+library(gdata);library(Nippon)
 options(download.file.method='libcurl')
 
 perl <- gdata:::findPerl('perl')
 dataURL <-
   c('http://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001w3y-att/historical-jika.xls')
   Sys.sleep(1) #avoid to overload
-  objcolumn <- c(2:8)
   buf0 <-
     read.xls(
       dataURL[1],
@@ -16,12 +15,15 @@ dataURL <-
       header = F,
       stringsAsFactors = F,
       sheet = 1,
-      fileEncoding = 'utf-8'
-    )
-  buf1 <- buf0[, c(1, objcolumn)]
-  colnames(buf1) <-  paste0(buf1[1,1],'-',gsub('\n','',unlist(buf1[3, ])))
-  buf2 <- buf1[-c(1:3), ]
+      fileEncoding = 'utf-8')
+  buf1 <- buf0[,!is.na(buf0[1,])]
+  obj.col <- which(gsub('[a-zA-Z0-9\n]+|\\s','',buf1[3,])=='')
+  buf1[3,-obj.col] <- gsub('([^a-zA-Z0-9\n]+).+','\\1',buf1[3,-obj.col])
+  colnames(buf1) <-  paste0(gsub('[a-zA-Z0-9\n]+|\\s','',buf1[1,1]),':',buf1[3,])
+  buf2 <- buf1[!is.na(as.numeric(gsub(',','',buf1[,2]))),]
   buf2[,-1] <- apply(buf2[,-1],2,function(x)as.numeric(gsub(',','',x))*10^-6) # 兆円
+  colnames(buf2)[-1] <- sapply(paste0(colnames(buf2)[-1],'(兆円)'),zen2han)
   buf2[,1]  <- as.Date(buf2[,1])
   colnames(buf2)[1] <- 'Date'
+  row.names(buf2) <- NULL
   assign(paste0('marketCapitalization'), buf2, envir = .GlobalEnv)
